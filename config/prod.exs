bf52636af44cdf9c3cf544f9e2af22b9f3052aff
Boris Babic <boris.ivan.babic@gmail.com>
2019-11-29 16:04:01 +0100

    Setup heroku

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :backend, BackendWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "fast-cove-73316", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
