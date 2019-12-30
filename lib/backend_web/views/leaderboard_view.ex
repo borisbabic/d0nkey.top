bc18424625eaa55c34fe488eed645fe520ad1f6b
Boris Babic <boris.ivan.babic@gmail.com>
2019-12-30 07:38:58 +0100

    Add support for highlighted players

--- lib/backend_web/views/leaderboard_view.ex
+++ lib/backend_web/views/leaderboard_view.ex
@@ -7,42 +7,14 @@ defmodule BackendWeb.LeaderboardView do
         conn: conn,
         region: region,
         leaderboard_id: leaderboard_id,
-        updated_at: updated_at
+        updated_at: updated_at,
+        highlight: highlighted_raw
       }) do
-    invited =
-      MapSet.new(
-        invited_raw,
-        fn ip ->
-          String.splitter(ip.battletag_full, "#")
-          |> Enum.at(0)
-          |> to_string()
-        end
-      )
-
-    # add qualified and qualifying
-    # add qualified
-    {entry, _} =
-      Enum.map_reduce(entry_raw, 0, fn le = %{battletag: battletag}, acc ->
-        qualified = MapSet.member?(invited, to_string(battletag))
-        qualifying = !qualified && acc < 16
-
-        {Map.put_new(le, :qualified, qualified)
-         |> Map.put_new(:qualifying, qualifying),
-         if qualified do
-           acc
-         else
-           acc + 1
-         end}
-      end)
-
+    invited = process_invited(invited_raw)
+    entry = process_entry(entry_raw, invited)
+    highlighted = process_highlighted(highlighted_raw, entry)
     old = updated_at && DateTime.diff(DateTime.utc_now(), updated_at) > 3600
-
-    updated_at_string =
-      updated_at
-      |> DateTime.to_iso8601()
-      |> String.splitter(".")
-      |> Enum.at(0)
-      |> String.replace("T", " ")
+    updated_at_string = process_updated_at(updated_at)
 
     render("index.html", %{
       conn: conn,
@@ -50,7 +22,55 @@ defmodule BackendWeb.LeaderboardView do
       region: region,
       leaderboard_id: leaderboard_id,
       old: old,
-      updated_at: updated_at_string
+      updated_at: updated_at_string,
+      highlighted: highlighted
     })
+  end
+
+  def process_updated_at(updated_at) do
+    updated_at
+    |> DateTime.to_iso8601()
+    |> String.splitter(".")
+    |> Enum.at(0)
+    |> String.replace("T", " ")
+  end
+
+  def process_invited(invited_raw) do
+    MapSet.new(
+      invited_raw,
+      fn ip ->
+        String.splitter(ip.battletag_full, "#")
+        |> Enum.at(0)
+        |> to_string()
+      end
+    )
+  end
+
+  def process_highlighted(highlighted_raw, entry) do
+    is_highlighted = fn %{battletag: battletag} ->
+      MapSet.member?(highlighted_raw, to_string(battletag))
+    end
+
+    if highlighted_raw && Enum.any?(entry, is_highlighted) do
+      Enum.filter(entry, is_highlighted)
+    else
+      nil
+    end
+  end
+
+  def process_entry(entry_raw, invited) do
+    Enum.map_reduce(entry_raw, 0, fn le = %{battletag: battletag}, acc ->
+      qualified = MapSet.member?(invited, to_string(battletag))
+      qualifying = !qualified && acc < 16
+
+      {Map.put_new(le, :qualified, qualified)
+       |> Map.put_new(:qualifying, qualifying),
+       if qualified do
+         acc
+       else
+         acc + 1
+       end}
+    end)
+    |> elem(0)
   end
 end
