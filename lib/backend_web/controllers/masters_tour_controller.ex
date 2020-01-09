970a99547a0dacc025aea437e9cbde68e913e702
Boris Babic <boris.ivan.babic@gmail.com>
2020-01-09 17:10:23 +0100

    Enhance battlefy communication

--- lib/backend_web/controllers/masters_tour_controller.ex
+++ lib/backend_web/controllers/masters_tour_controller.ex
@@ -1,10 +1,16 @@
 defmodule BackendWeb.MastersTourController do
   use BackendWeb, :controller
   alias Backend.MastersTour
+  alias Backend.Infrastructure.BattlefyCommunicator
 
   def invited_players(conn, %{"tour_stop" => tour_stop}) do
     MastersTour.fetch()
     invited = MastersTour.list_invited_players(tour_stop)
     render(conn, "invited_players.html", %{invited: invited})
+  end
+
+  def qualifiers(conn, _params) do
+    fetched = BattlefyCommunicator.get_masters_qualifiers()
+    render(conn, "qualifiers.html", %{fetched_qualifiers: fetched})
   end
 end
