ec66265b5e6589f1c09dff380c176cc1f9a877ae
Boris Babic <boris.ivan.babic@gmail.com>
2020-01-09 09:37:17 +0100

    Fetch invited_players on each load

--- lib/backend_web/controllers/masters_tour_controller.ex
+++ lib/backend_web/controllers/masters_tour_controller.ex
@@ -3,6 +3,7 @@ defmodule BackendWeb.MastersTourController do
   alias Backend.MastersTour
 
   def invited_players(conn, %{"tour_stop" => tour_stop}) do
+    MastersTour.fetch()
     invited = MastersTour.list_invited_players(tour_stop)
     render(conn, "invited_players.html", %{invited: invited})
   end
