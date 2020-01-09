7d5268ba344fc305e909b2969af4f7b85999345f
Boris Babic <boris.ivan.babic@gmail.com>
2020-01-09 12:17:12 +0100

    Remove old warning for old seasons

--- lib/backend_web/views/leaderboard_view.ex
+++ lib/backend_web/views/leaderboard_view.ex
@@ -15,29 +15,9 @@ defmodule BackendWeb.LeaderboardView do
     invited = process_invited(invited_raw, updated_at_string)
     entry = process_entry(entry_raw, invited)
     highlighted = process_highlighted(highlighted_raw, entry)
-    old = updated_at && DateTime.diff(DateTime.utc_now(), updated_at) > 3600
     season_id = conn.query_params["seasonId"]
-    # todo generate these from the current date
-    selectable_seasons = [{"JAN", 75}, {"DEC", 74}, {"NOV", 73}]
-
-    crystal =
-      case leaderboard_id do
-        "STD" ->
-          "https://d2q63o9r0h0ohi.cloudfront.net/images/leaderboards/crystal_standard-add3c953a625a04c8545699c65c338786606c56e770182c236c7ec5229bf5f1e78631e57bcdda6eee820f3a13e57e97fe22f0e39b5777c7e41b75ce28f3bd8c7.png"
-
-        "BG" ->
-          "https://d2q63o9r0h0ohi.cloudfront.net/images/leaderboards/crystal_battlegrounds-5cd82d919afcfc5de20e0857cfce3e19ba9bd47d8f02ab977d3fa3a17b9dc7c972a18e0f55eb970ff0639aa69045b3aacb3cc1125d17a9550bd5ed7167a51aea.png"
-
-        "WLD" ->
-          "https://d2q63o9r0h0ohi.cloudfront.net/images/leaderboards/crystal_wild-f9075a1fe0a5953b314fab5ca15f7cc83db86764786f590b8d64fb87603f797adbfd75ffd6160d89bf53ae08eb50d032a3d9d6885c0e03b0fcd6f22265aa6a0f.png"
-      end
-
-    season_selector = fn season_tuple ->
-      to_string(elem(season_tuple, 1)) == to_string(season_id)
-    end
-
-    season_name =
-      Enum.find(selectable_seasons, Enum.at(selectable_seasons, 0), season_selector) |> elem(0)
+    {selectable_seasons, season_name, is_latest_season} = handle_seasons(season_id)
+    old = updated_at && DateTime.diff(DateTime.utc_now(), updated_at) > 3600 && is_latest_season
 
     render("index.html", %{
       conn: conn,
@@ -50,8 +30,35 @@ defmodule BackendWeb.LeaderboardView do
       season_id: season_id,
       selectable_seasons: selectable_seasons,
       season_name: season_name,
-      crystal: crystal
+      crystal: get_crystal(leaderboard_id)
     })
+  end
+
+  def get_crystal(leaderboard_id) do
+    case leaderboard_id do
+      "STD" ->
+        "https://d2q63o9r0h0ohi.cloudfront.net/images/leaderboards/crystal_standard-add3c953a625a04c8545699c65c338786606c56e770182c236c7ec5229bf5f1e78631e57bcdda6eee820f3a13e57e97fe22f0e39b5777c7e41b75ce28f3bd8c7.png"
+
+      "BG" ->
+        "https://d2q63o9r0h0ohi.cloudfront.net/images/leaderboards/crystal_battlegrounds-5cd82d919afcfc5de20e0857cfce3e19ba9bd47d8f02ab977d3fa3a17b9dc7c972a18e0f55eb970ff0639aa69045b3aacb3cc1125d17a9550bd5ed7167a51aea.png"
+
+      "WLD" ->
+        "https://d2q63o9r0h0ohi.cloudfront.net/images/leaderboards/crystal_wild-f9075a1fe0a5953b314fab5ca15f7cc83db86764786f590b8d64fb87603f797adbfd75ffd6160d89bf53ae08eb50d032a3d9d6885c0e03b0fcd6f22265aa6a0f.png"
+    end
+  end
+
+  def handle_seasons(season_id) do
+    # todo generate these from the current date
+    selectable_seasons = [{"JAN", 75}, {"DEC", 74}, {"NOV", 73}]
+    latest = {_, latest_season_id} = Enum.at(selectable_seasons, 0)
+
+    season_selector = fn season_tuple ->
+      to_string(elem(season_tuple, 1)) == to_string(season_id)
+    end
+
+    {season_name, curr_season_id} = Enum.find(selectable_seasons, latest, season_selector)
+
+    {selectable_seasons, season_name, curr_season_id == latest_season_id}
   end
 
   def process_updated_at(_ = nil) do
