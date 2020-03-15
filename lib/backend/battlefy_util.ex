b0a544777903f1949a8a5d041e4aad120924472a
Boris Babic <boris.ivan.babic@gmail.com>
2020-03-15 21:51:01 +0100

    Add helper funcs to traverse matches brackets

--- lib/backend/battlefy_util.ex
+++ lib/backend/battlefy_util.ex
@@ -37,6 +37,7 @@ defmodule Backend.BattlefyUtil do
   def group_and_get_standings(tournaments) do
     tournaments
     |> Enum.map(&get_standings_and_duration/1)
+    |> Enum.filter(fn %{duration: duration} -> duration end)
     |> Enum.group_by(fn tds -> tds.tournament.region end)
   end
 
@@ -72,6 +73,7 @@ defmodule Backend.BattlefyUtil do
     total =
       t
       |> Enum.map(fn twm -> twm.duration end)
+      |> Enum.filter(&Util.id/1)
       |> Enum.sum()
 
     total / count
@@ -97,7 +99,8 @@ defmodule Backend.BattlefyUtil do
     tournaments |> Enum.map(fn %{standings: s} -> s |> Enum.count() end) |> Enum.min_max()
   end
 
-  def get_meta(t = _tournaments_with_meta) do
+  def get_meta(unfiltered = _tournaments_with_meta) do
+    t = unfiltered |> Enum.filter(fn %{duration: duration} -> duration end)
     {fewest, most} = min_max_players(t)
 
     %{
@@ -113,5 +116,86 @@ defmodule Backend.BattlefyUtil do
       num_fewer_rounds: get_num_short(t),
       num_full: get_num_full(t)
     }
+  end
+
+  def get_meta_per_region_for_tour(tour_stop) do
+    Backend.MastersTour.get_qualifiers_for_tour(tour_stop)
+    |> Enum.map(fn t -> Backend.Battlefy.get_tournament(t.id) end)
+    |> Backend.BattlefyUtil.group_and_get_standings()
+    |> Enum.map(fn {region, tours} -> {region, Backend.BattlefyUtil.get_meta(tours)} end)
+  end
+
+  def get_prev_rounds_matches(1, _rounds) do
+    0
+  end
+
+  @doc """
+    Gets the number of matches that happened in previous rounds
+    ## Example
+      iex> Backend.BattlefyUtil.get_prev_rounds_matches(3, 9)
+      384
+  """
+  def get_prev_rounds_matches(round_num, total_rounds) do
+    (get_prev_rounds_matches(round_num - 1, total_rounds) +
+       :math.pow(2, total_rounds - round_num + 1))
+    |> trunc()
+  end
+
+  @doc """
+    Gets the match num for a position in a round
+    ## Example
+      iex> Backend.BattlefyUtil.get_matchnum(5, 3, 9)
+      389
+      iex> Backend.BattlefyUtil.get_matchnum(88, 1, 9)
+      88
+  """
+  def get_matchnum(pos_in_round, round_num, total_rounds) do
+    get_prev_rounds_matches(round_num, total_rounds) + pos_in_round
+  end
+
+  @doc """
+    Gets the position of a match in a round
+    ## Example
+      iex> Backend.BattlefyUtil.get_pos_in_round(389, 3, 9)
+      5
+      iex> Backend.BattlefyUtil.get_pos_in_round(300, 2, 9)
+      44
+  """
+  def get_pos_in_round(match_num, round_num, total_rounds) do
+    match_num - get_prev_rounds_matches(round_num, total_rounds)
+  end
+
+  def prev_top(_match_num, 1, _total_rounds) do
+    nil
+  end
+
+  @doc """
+    Gets the matchnum of the match the top player comes from
+    ## Example
+      iex> Backend.BattlefyUtil.prev_top(389, 3, 9)
+      265
+      iex> Backend.BattlefyUtil.prev_top(300, 2, 9)
+      87
+  """
+  def prev_top(match_num, round_num, total_rounds) do
+    pos_in_round = get_pos_in_round(match_num, round_num, total_rounds)
+    get_matchnum(pos_in_round * 2 - 1, round_num - 1, total_rounds)
+  end
+
+  def prev_bottom(_match_num, 1, _total_rounds) do
+    nil
+  end
+
+  @doc """
+    Gets the matchnum of the match the bottom player comes from
+    ## Example
+      iex> Backend.BattlefyUtil.prev_bottom(389, 3, 9)
+      266
+      iex> Backend.BattlefyUtil.prev_bottom(300, 2, 9)
+      88
+  """
+  def prev_bottom(match_num, round_num, total_rounds) do
+    pos_in_round = get_pos_in_round(match_num, round_num, total_rounds)
+    get_matchnum(pos_in_round * 2, round_num - 1, total_rounds)
   end
 end
