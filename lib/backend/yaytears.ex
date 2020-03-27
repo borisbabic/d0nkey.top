66e664bd4181239b3b6ea3356aca53e0ea016c6a
Boris Babic <boris.ivan.babic@gmail.com>
2020-03-27 23:14:10 +0100

    Add way to view future opponents

--- lib/backend/yaytears.ex
+++ lib/backend/yaytears.ex
@@ -1,5 +1,5 @@
 defmodule Backend.Yaytears do
-  def create_deckstrings_link(battlefy_id, battletag_full) do
-    "https://www.yaytears.com/battlefy/#{battlefy_id}/#{battletag_full |> URI.encode_www_form()}"
+  def create_deckstrings_link(tournament_id, battletag_full) do
+    "https://www.yaytears.com/battlefy/#{tournament_id}/#{battletag_full |> URI.encode_www_form()}"
   end
 end
--- lib/backend_web/controllers/battlefy_controller.ex
+++ lib/backend_web/controllers/battlefy_controller.ex
@@ -29,4 +29,29 @@ defmodule BackendWeb.BattlefyController do
     link = Backend.HSDeckViewer.create_link(tournament_decks)
     redirect(conn, external: link)
   end
+
+  def future_opponents(conn, %{
+        "tournament_id" => tournament_id,
+        "battletag_full" => team_name
+      }) do
+    future_opponents = Battlefy.get_future_opponents(tournament_id, team_name)
+
+    render(conn, "future_opponents.html", %{
+      tournament_id: tournament_id,
+      future_opponents: future_opponents,
+      team_name: team_name,
+      conn: conn
+    })
+  end
+
+  # def tournament_matches(conn, %{
+  #       "tournament_id" => tournament_id,
+  #       "stage_id" => stage_id,
+  #       "round" => round,
+  #     }) do
+  #   # tournament_matches =
+  #   #   Battlefy.get_matches()
+  #   # link = Backend.HSDeckViewer.create_link(tournament_decks)
+  #   # redirect(conn, external: link)
+  # end
 end
