c15a432fbb53d90a4475f9e7c394ee2fb8207754
Boris Babic <boris.ivan.babic@gmail.com>
2020-07-16 02:11:59 +0200

    Fix qualifiers signed up checking

--- lib/backend/battlefy/organization.ex
+++ lib/backend/battlefy/organization.ex
@@ -5,8 +5,8 @@ defmodule Backend.Battlefy.Organization do
 
   typedstruct enforce: true do
     field :id, Battlefy.organization_id()
-    field :name, String.t()
-    field :owner_id, Battlefy.user_id()
+    field :name, String.t() | nil
+    field :owner_id, Battlefy.user_id() | nil
     field :slug, String.t()
     field :banner_url, String.t() | nil
     field :logo_url, String.t() | nil
@@ -21,16 +21,19 @@ defmodule Backend.Battlefy.Organization do
     |> from_raw_map
   end
 
-  def from_raw_map(map = %{"owner_id" => owner_id, "name" => name, "slug" => slug}) do
+  def from_raw_map(map = %{"slug" => slug}) do
     %__MODULE__{
       id: map["_id"] || map["id"],
-      name: name,
+      name: map["name"],
       slug: slug,
-      owner_id: owner_id,
+      owner_id: map["owner_id"],
       banner_url: map["banner_url"],
       logo_url: map["logo_url"]
     }
   end
+
+  def display_name(%{name: nil, slug: slug}), do: slug
+  def display_name(%{name: name}), do: name
 
   def create_link(%{slug: slug}) do
     "https://battlefy.com/#{slug}"
