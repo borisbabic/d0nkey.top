31d13d4ce66e6a7e4b33286e70cf15b8a219c110
Boris Babic <boris.ivan.babic@gmail.com>
2020-08-06 00:37:23 +0200

    Use single event listener for tz conversion

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,10 +22,29 @@ new ClipboardJS(".clip-btn-value", {text: function(trigger) {
     return trigger.getAttribute("aria-label");
 }});
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("test")
     var hideWithJs = document.getElementsByClassName("is-shown-js")
     for (var i = 0; i < hideWithJs.length; i++) {
         hideWithJs[i].style.display = "";
     }
 })
 
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Converting datetimes")
+    var pad = function(te) {
+        return ('0' + te).slice(-2)
+    }
+    var toConvert = document.getElementsByClassName("datetime-human")
+    for (var i = 0; i < toConvert.length; i++) {
+        try {
+            var timestamp = toConvert[i].getAttribute("aria-label");
+            var date = new Date(parseInt(timestamp));
+            if (date.getMonth()) {
+                toConvert[i].innerHTML =
+                    [date.getFullYear(), pad(date.getMonth() + 1), pad(date.getDate())].join('-')
+                    + ' '
+                    + [ pad(date.getHours()), pad(date.getMinutes()), pad(date.getSeconds())].join(':')
+            }
+        }
+        catch(e){ }
+    }
+})
