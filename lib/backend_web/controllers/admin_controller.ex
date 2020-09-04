439cab4a0bc1bba15f53b9fe2a2b7d7c54cdd313
Boris Babic <boris.ivan.babic@gmail.com>
2020-09-04 09:45:50 +0200

    Fix admin route

--- lib/backend_web/controllers/admin_controller.ex
+++ lib/backend_web/controllers/admin_controller.ex
@@ -4,12 +4,14 @@ defmodule BackendWeb.AdminController do
   def get_all_leaderboards(conn, _params) do
     current_season = Date.utc_today() |> Backend.Blizzard.get_season_id()
 
-    for ldb <- ["WLD", "STD"],
-        season_id <- 63..current_season,
-        region <- Backend.Blizzard.qualifier_regions() do
-      Backend.Leaderboards.get_leaderboard(region, ldb, season_id)
-    end
+    Task.start(fn ->
+      for ldb <- ["WLD", "STD"],
+          season_id <- 63..current_season,
+          region <- Backend.Blizzard.qualifier_regions() do
+        Backend.Leaderboards.get_leaderboard(region, ldb, season_id)
+      end
+    end)
 
-    "Success!"
+    text(conn, "Success")
   end
 end
