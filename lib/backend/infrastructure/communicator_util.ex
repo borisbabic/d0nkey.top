a2c4f79248c8af7fb817c33fd7891f17b39a429d
Boris Babic <boris.ivan.babic@gmail.com>
2020-11-27 12:51:58 +0100

    Reduce request logging

--- lib/backend/infrastructure/communicator_util.ex
+++ lib/backend/infrastructure/communicator_util.ex
@@ -9,7 +9,7 @@ defmodule Backend.Infrastructure.CommunicatorUtil do
 
   def get_response(url) do
     {u_secs, response} = :timer.tc(&HTTPoison.get!/1, [URI.encode(url)])
-    Logger.info("Got #{url} in #{div(u_secs, 1000)} ms")
+    Logger.debug("Got #{url} in #{div(u_secs, 1000)} ms")
     response
   end
 end
