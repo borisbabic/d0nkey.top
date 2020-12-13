4ffb559484e99bc68451c605387f918528632355
Boris Babic <boris.ivan.babic@gmail.com>
2020-12-13 14:26:31 +0100

    Add deckcode embiggener

---
+++ lib/backend/hearthstone/deckcode_embiggener.ex
@@ -0,0 +1,47 @@
+defmodule Backend.Hearthstone.DeckcodeEmbiggener do
+  @moduledoc false
+  alias Backend.HearthstoneJson
+  alias Backend.Hearthstone.Deck
+
+  @doc """
+  Takes a deck or deckcode and produces a long form deckcode
+  """
+  @spec embiggen(Deck.t() | String.t()) :: String.t()
+  def embiggen(deckcode) when is_binary(deckcode), do: deckcode |> Deck.decode() |> embiggen()
+
+  def embiggen(d = %{cards: cards, deckcode: deckcode, format: format}) do
+    class_name = d |> class_name()
+
+    cards_part =
+      cards
+      |> Enum.frequencies()
+      |> Enum.map(fn {c, freq} ->
+        {HearthstoneJson.get_card(c), freq}
+      end)
+      |> Enum.sort_by(fn {c, _} -> c.name end)
+      |> Enum.sort_by(fn {c, _} -> c.cost end)
+      |> Enum.map(fn {c, freq} ->
+        "# #{freq}x (#{c.cost}) #{c.name}"
+      end)
+      |> Enum.join("\n")
+
+    """
+    ### #{class_name}
+    # Class: #{class_name}
+    # Format: #{format |> Deck.format_name()}
+    # 
+    #{cards_part}
+    #
+    #{deckcode}
+    #
+    # To use this deck, copy it to your clipboard and create a new deck in Hearthstone
+    """
+  end
+
+  @spec class_name(Deck.t()) :: String.t()
+  defp class_name(d) do
+    (d.class || HearthstoneJson.get_class(d.hero))
+    |> String.upcase()
+    |> Deck.class_name()
+  end
+end
