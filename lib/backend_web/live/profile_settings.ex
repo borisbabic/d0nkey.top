a8f503776b73300ab5d3e00fa677678ef8df81fd
Boris Babic <boris.ivan.babic@gmail.com>
2021-02-15 23:13:34 +0100

    Make profile settings nicer

--- lib/backend_web/live/profile_settings.ex
+++ lib/backend_web/live/profile_settings.ex
@@ -27,14 +27,34 @@ defmodule BackendWeb.ProfileSettingsLive do
         <div :if={{ @user }}>
           <Form for={{ :user }} submit="submit">
             <Field name="battlefy_slug">
-              <Label>Battlefy Slug</Label>
-              <TextInput value={{ @user.battlefy_slug }}/>
-              <Label>Open your battlefy profile then paste the url and I'll extract it</Label>
+              <div class="level"> 
+                <div class="level-left">
+                  <div class="level-item">
+                    <TextInput class="input is-small" value={{ @user.battlefy_slug }}/>
+                  </div>
+                  <div class="level-item">
+                    <Label class="label" >Battlefy Slug</Label>
+                  </div>
+                  <div class="level-item">
+                    <Label class="label" >Open your battlefy profile then paste the url and I'll extract it</Label>
+                  </div>
+                </div>
+              </div>
             </Field>
             <Field name="country_code">
-              <Label><a href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2" target="_blank">alpha2 country code</a></Label>
-              <TextInput value={{ @user.country_code }}/>
-              <ErrorTag field="country_code"/>
+              <div class="level"> 
+                <div class="level-left">
+                  <div class="level-item">
+                    <TextInput value={{ @user.country_code }} class="input is-small" />
+                  </div>
+                  <div class="level-item">
+                    <Label class="label" >Enter 2 character <a href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2" target="_blank">alpha2 country code</a></Label>
+                  </div>
+                  <div class="level-item">
+                    <ErrorTag field="country_code"/>
+                  </div>
+                </div>
+              </div>
             </Field>
             <Submit label="Save" class="button"/>
           </Form>
