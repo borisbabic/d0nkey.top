4b0f7e0098d01c67b3fe7f7b13686a3a341ddbd1
Boris Babic <boris.ivan.babic@gmail.com>
2021-02-28 13:06:48 +0100

    Sign out users with invalid token

--- lib/backend/user_manager/error_handler.ex
+++ lib/backend/user_manager/error_handler.ex
@@ -1,9 +1,16 @@
 defmodule Backend.UserManager.ErrorHandler do
+  @moduledoc false
   import Plug.Conn
 
   @behaviour Guardian.Plug.ErrorHandler
 
   @impl Guardian.Plug.ErrorHandler
+  def auth_error(conn, {:invalid_token, _}, _) do
+    conn
+    |> Backend.UserManager.Guardian.Plug.sign_out()
+    |> Phoenix.Controller.redirect(to: "/")
+  end
+
   def auth_error(conn, {type, _reason}, _opts) do
     body = to_string(type)
 
