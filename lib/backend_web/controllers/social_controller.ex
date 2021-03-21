bca1b27f08e8db7a09287cf271b7c433693619e4
Boris Babic <boris.ivan.babic@gmail.com>
2021-03-22 00:42:31 +0100

    Add social links

---
+++ lib/backend_web/controllers/social_controller.ex
@@ -0,0 +1,31 @@
+defmodule BackendWeb.SocialController do
+  use BackendWeb, :controller
+
+  def discord(conn, _) do
+    redirect(conn, external: Constants.discord())
+  end
+
+  def paypal(conn, _) do
+    redirect(conn, external: Constants.paypal())
+  end
+
+  def twitch(conn, _) do
+    redirect(conn, external: Constants.twitch())
+  end
+
+  def patreon(conn, _) do
+    redirect(conn, external: Constants.patreon())
+  end
+
+  def twitter(conn, _) do
+    redirect(conn, external: Constants.twitter())
+  end
+
+  def notion(conn, _) do
+    redirect(conn, external: Constants.notion())
+  end
+
+  def liberapay(conn, _) do
+    redirect(conn, external: Constants.liberapay())
+  end
+end
