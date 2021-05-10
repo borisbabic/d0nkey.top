473086f61148687c3a581a5c579cb75cf9aa86c6
Boris Babic <boris.ivan.babic@gmail.com>
2021-05-10 17:41:25 +0200

    Finish gm lineup filtering

---
+++ lib/backend_web/components/filter/playable_card_select.ex
@@ -0,0 +1,65 @@
+defmodule Components.Filter.PlayableCardSelect do
+  @moduledoc false
+  use Surface.LiveComponent
+  alias Components.Dropdown
+  alias Surface.Components.Form
+  alias Surface.Components.Form.TextInput
+
+  prop(update_fun, :fun)
+  prop(selected, :list, default: [])
+  prop(title, :string, default: "Select cards")
+  prop(search, :string, default: "")
+
+  def render(assigns) do
+    ~H"""
+      <Dropdown title="{{ @title }}">
+        <Form for={{ :search }} change="search" submit="search" opts={{ autocomplete: "off" }}>
+          <div class="columns is-mobile is-multiline">
+            <div class="column is-narrow">
+              <TextInput class="input" opts={{ placeholder: "Search" }}/>
+            </div>
+          </div>
+        </Form>
+        <a class="dropdown-item is-active" :on-click="remove_card" :for={{ selected <- @selected }} phx-value-card={{ selected }}>
+          {{ Backend.HearthstoneJson.get_card(selected).name }}
+        </a>
+        <a class="dropdown-item" :for={{ card <- cards(@search, @selected) }} :on-click="add_card" phx-value-card={{ card.dbf_id }}>
+          {{ card.name }}
+        </a>
+      </Dropdown>
+
+    """
+  end
+
+  def handle_event("search", %{"search" => [search]}, socket),
+    do: {:noreply, assign(socket, :search, search)}
+
+  def handle_event(
+        "remove_card",
+        %{"card" => card},
+        socket = %{assigns: %{update_fun: update_fun, selected: selected}}
+      ) do
+    {dbf_id, _} = Integer.parse(card)
+    update_fun.(selected -- [dbf_id])
+    {:noreply, socket}
+  end
+
+  def handle_event(
+        "add_card",
+        %{"card" => card},
+        socket = %{assigns: %{update_fun: update_fun, selected: selected}}
+      ) do
+    {dbf_id, _} = Integer.parse(card)
+    update_fun.([dbf_id | selected])
+    {:noreply, socket}
+  end
+
+  def cards(search, selected) do
+    num_to_show = (7 - Enum.count(selected)) |> max(3)
+
+    Backend.HearthstoneJson.playable_cards()
+    |> Enum.filter(&(String.downcase(&1.name) =~ String.downcase(search)))
+    |> Enum.filter(&(!(&1.dbf_id in selected)))
+    |> Enum.take(num_to_show)
+  end
+end
