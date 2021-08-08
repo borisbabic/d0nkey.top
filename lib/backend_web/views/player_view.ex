5dee7778fd70eb7767a0923ad6cd8514e11ec807
Boris Babic <boris.ivan.babic@gmail.com>
2021-08-09 01:14:42 +0200

    Add current mtqs to player profile and hide useless

--- lib/backend_web/views/player_view.ex
+++ lib/backend_web/views/player_view.ex
@@ -24,17 +24,20 @@ defmodule BackendWeb.PlayerView do
           finishes: finishes,
           competitions: competitions,
           tournament_team_stats: tts,
-          conn: conn,
-          mt_earnings: mt_earnings
+          conn: conn
         }
       ) do
     stats_rows =
       qs
-      |> Enum.flat_map(fn {year, ps} ->
-        [
-          {"#{year} MTQ played", ps |> PlayerStats.with_result()},
-          {"#{year} MTQ winrate", ps |> PlayerStats.matches_won_percent() |> Float.round(2)}
-        ]
+      |> Enum.flat_map(fn {period, ps} ->
+        if ps |> PlayerStats.with_result() > 0 do
+          [
+            {"#{period} MTQ played", ps |> PlayerStats.with_result()},
+            {"#{period} MTQ winrate", ps |> PlayerStats.matches_won_percent() |> Float.round(2)}
+          ]
+        else
+          []
+        end
       end)
 
     player_rows =
@@ -55,15 +58,18 @@ defmodule BackendWeb.PlayerView do
           ]
       end
 
-    earnings_rows = [{"2020 MT earnings", mt_earnings}]
-
     mt_total_stats =
       tts |> Enum.map(&TournamentTeamStats.total_stats/1) |> TeamStats.calculate_team_stats()
 
-    mt_total_row = [{"MT Match Score", "#{mt_total_stats.wins} - #{mt_total_stats.losses}"}]
+    mt_total_row =
+      if mt_total_stats.wins + mt_total_stats.losses > 0 do
+        [{"MT Match Score", "#{mt_total_stats.wins} - #{mt_total_stats.losses}"}]
+      else
+        []
+      end
 
     rows =
-      (player_rows ++ stats_rows ++ earnings_rows ++ mt_total_row)
+      (player_rows ++ stats_rows ++ mt_total_row)
       |> Enum.map(fn {title, val} -> "#{title}: #{val}" end)
 
     table_headers =
@@ -96,73 +102,11 @@ defmodule BackendWeb.PlayerView do
         }
       end)
 
-    qualifier_rows =
-      t
-      |> Enum.flat_map(fn t ->
-        t.standings
-        |> Enum.find(fn ps -> ps.battletag_full == battletag_full end)
-        |> case do
-          # this shouldn't be possible, but let's be safe
-          nil ->
-            []
-
-          ps ->
-            tournament_link = Routes.battlefy_path(conn, :tournament, t.tournament_id)
-
-            tournament_title = Recase.to_title(t.tournament_slug)
-
-            player_link =
-              Routes.battlefy_path(conn, :tournament_player, t.tournament_id, battletag_full)
-
-            [
-              %{
-                competition: simple_link(tournament_link, tournament_title),
-                time: t.start_time,
-                position: simple_link(player_link, ps.position),
-                score: "#{ps.wins} - #{ps.losses}"
-              }
-            ]
-        end
-      end)
+    qualifier_rows = qualifier_rows(t, battletag_full, conn)
 
     leaderboard_names = Backend.PlayerInfo.leaderboard_names(battletag_full)
 
-    leaderboard_rows =
-      finishes
-      |> Enum.flat_map(fn f ->
-        f.entries
-        |> Enum.find(fn e -> leaderboard_names |> Enum.member?(e.account_id) end)
-        |> case do
-          # this shouldn't be possible, but let's be safe
-          nil ->
-            []
-
-          pe ->
-            leaderboard_link =
-              Routes.leaderboard_path(
-                conn,
-                :index,
-                %{
-                  "leaderboardId" => f.leaderboard_id,
-                  "seasonId" => f.season_id,
-                  "region" => f.region
-                  #                   "highlight" => [pe.account_id |> Backend.MastersTour.InvitedPlayer.shorten_battletag()]
-                }
-              )
-
-            leaderboard_title =
-              Blizzard.get_leaderboard_name(f.region, f.leaderboard_id, f.season_id)
-
-            [
-              %{
-                competition: simple_link(leaderboard_link, leaderboard_title),
-                time: f.upstream_updated_at,
-                position: simple_link(leaderboard_link, pe.rank),
-                score: pe.rating || ""
-              }
-            ]
-        end
-      end)
+    leaderboard_rows = leaderboard_rows(finishes, leaderboard_names, conn)
 
     table_rows =
       pick_competitions(competitions, %{
@@ -190,6 +134,74 @@ defmodule BackendWeb.PlayerView do
       competition_options: get_competition_options(competitions),
       table_rows: table_rows
     })
+  end
+
+  def qualifier_rows(tournaments, battletag_full, conn) do
+    tournaments
+    |> Enum.flat_map(fn t ->
+      t.standings
+      |> Enum.find(fn ps -> ps.battletag_full == battletag_full end)
+      |> case do
+        # this shouldn't be possible, but let's be safe
+        nil ->
+          []
+
+        ps ->
+          tournament_link = Routes.battlefy_path(conn, :tournament, t.tournament_id)
+
+          tournament_title = Recase.to_title(t.tournament_slug)
+
+          player_link =
+            Routes.battlefy_path(conn, :tournament_player, t.tournament_id, battletag_full)
+
+          [
+            %{
+              competition: simple_link(tournament_link, tournament_title),
+              time: t.start_time,
+              position: simple_link(player_link, ps.position),
+              score: "#{ps.wins} - #{ps.losses}"
+            }
+          ]
+      end
+    end)
+  end
+
+  def leaderboard_rows(finishes, leaderboard_names, conn) do
+    finishes
+    |> Enum.flat_map(fn f ->
+      f.entries
+      |> Enum.find(fn e -> leaderboard_names |> Enum.member?(e.account_id) end)
+      |> case do
+        # this shouldn't be possible, but let's be safe
+        nil ->
+          []
+
+        pe ->
+          leaderboard_link =
+            Routes.leaderboard_path(
+              conn,
+              :index,
+              %{
+                "leaderboardId" => f.leaderboard_id,
+                "seasonId" => f.season_id,
+                "region" => f.region
+                #                   "highlight" => [pe.account_id |> Backend.MastersTour.InvitedPlayer.shorten_battletag()]
+              }
+            )
+
+          leaderboard_title =
+            Blizzard.get_leaderboard_name(f.region, f.leaderboard_id, f.season_id)
+
+          [
+            %{
+              competition: simple_link(leaderboard_link, leaderboard_title),
+              time: f.upstream_updated_at,
+              position: simple_link(leaderboard_link, pe.rank),
+              score: pe.rating || ""
+            }
+          ]
+      end
+    end)
   end
 
   def get_competition_options(competitions) do
