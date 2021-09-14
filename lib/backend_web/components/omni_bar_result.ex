9a6ad055d3ac54f8a6c3957539cba523e3b1b355
Boris Babic <boris.ivan.babic@gmail.com>
2021-09-15 01:33:00 +0200

    Add wip omni bar

---
+++ lib/backend_web/components/omni_bar_result.ex
@@ -0,0 +1,13 @@
+defmodule Components.OmniBarResult do
+  @moduledoc "Displays a single omni bar result"
+
+  use Surface.Component
+
+  prop(result, :map, required: true)
+
+  def render(assigns) do
+    ~H"""
+      <a href={{ @result.link }}>{{@result.display_value}}</a>
+    """
+  end
+end
