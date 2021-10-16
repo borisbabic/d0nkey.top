895bced40820ea4fed70a12fa2f149895586d8ca
Boris <boris.ivan.babic@gmail.com>
2021-10-16 14:52:18 +0200

    Add more reqvam messages

--- lib/backend/reqvam_top100_tweeter.ex
+++ lib/backend/reqvam_top100_tweeter.ex
@@ -7,43 +7,65 @@ defmodule Backend.ReqvamTop100Tweeter do
     with {:ok, config} <- Application.fetch_env(:backend, :req_t100_twitter_info),
     :ok <- ExTwitter.configure(:process, config),
     %{entries: entries} <- Leaderboards.get_leaderboard("US", "STD", nil) do
-      message = case Enum.find(entries, & &1.rank <= 100 && &1.account_id == "reqvam") do
-        %{rank: 1} -> "DING! DING! DING! DING! Reqvam is #1! On NA! Maybe it's time to play on a tougher server?"
-        %{rank: rank} when rank < 10 -> "Reqvam is #{rank}! Wowza! One might think he is good at the game"
-        %{rank: rank} -> [
-          "Reqvam is top 100! He is currently #{rank}! I guess he isn't completely washed up",
-          "Reqvam is #{rank}! Or maybe it's actually Paul that is #{rank}",
-        ] |> Enum.random()
-        _ -> not_top_100_tweet()
-      end
+      message =
+        entries
+        |> Enum.find(entries, & &1.account_id == "reqvam")
+        |> msg(Date.utc_today())
+        |> pick_msg()
       ExTwitter.update(message)
     end
   end
 
-  defp not_top_100_tweet() do
-    date = Date.utc_today()
-    cond do
-      date.day < 5  ->
+  def pick_msg([msgs]), do: msgs |> Enum.random() |> pick_msg()
+  def pick_msg(msg), do: msg
+
+  def msg(%{rank: 69}, _date), do: "Nice!"
+  def msg(%{rank: 42}, _date), do: "\"What rank is reqvam on NA\" is probably not the ultimate question, but the answer is the same: 42"
+  def msg(%{rank: 1}, _date), do: "Reqvam is #1 ! On NA! Maybe it's time to play on a tougher server?"
+  def msg(%{rank: 24}, _date), do: "Reqvam is top 100, he is 4! ie 24, 4 factorial 🤓"
+  def msg(%{rank: 101}, _), do: "Reqvam is not top 100, he is 101. Ha! Ha!"
+  def msg(%{rank: rank}, _date) when rank < 100, do: top_100_messages(rank)
+  def msg(%{rank: rank}, %{day: 1, month: 1}) when rank < 100, do: "Reqvam is #{rank} ! Seems like somebody didn't have new years plans"
+  def msg(%{rank: rank}, %{day: 1}) when rank < 100, do: "Reqvam is #{rank} ! Seems like somebody had some free time to get to legend already"
+  def msg(%{rank: rank}, %{day: 2}) when rank < 100, do: "Reqvam is #{rank} ! But it's day 1 so is it even that good?"
+  def msg(%{rank: rank}, %{day: day}) when rank < 100 and day < 5, do: [
+    "Reqvam is #{rank} ! But is it really that impressive this early?",
+    "Reqvam is #{rank} ! But it's early, will it hold?"
+  ]
+  def msg(_entry, %{day: day}) when day < 5, do:
       [
         "Reqvam is not top 100, but it's still early",
         "The season recently started, maybe paul is keeping him busy? He isn't top 100 yet",
       ]
-      date.day <= 15 ->
+  def msg(_entry, %{day: day}) when day < 15, do:
         [
           "Not top 100. Nope.",
           "It's early, but it's not that early. Still not top 100??",
+          "Reqvam isn't top 100, so that means he is the perfect person to get some coaching from https://metafy.gg/@reqvam",
         ]
-      date.day > 15 ->
+  def msg(_entry, %{day: day}) when day > 15, do:
         [
           "What the heck? Not top 100! It's already today's date!!!",
           "Come on Paul, whatya doing! Reqvam aint top 100. You gotta help reqvam get a high rank so he can get those twitch views!",
+          "Reqvam isn't top 100, so that means he is the perfect person to get some coaching from https://metafy.gg/@reqvam",
         ]
-      date.day > 25 ->
+  def msg(_entry, %{day: day}) when day > 25, do:
         [
           "The season is almost over and reqvam isn't top 100? What a washed up player. Better find somebody else to watch on twitch",
           "Not top 100 this late in the month? Is reqvam even playing Standard anymore? Somebody go tell d0nkey to turn off this bot",
         ]
-    end
-    |> Enum.random()
-  end
+
+  def top_100_messages(rank), do:
+      [
+        "Reqvam is #{rank} ! That is top 100. What an amazing accomplishment by someone exactly worthy of such an accomplishment.",
+        "Reqvam is top 100 ! He is currently #{rank} ! I guess he isn't completely washed up",
+        "Reqvam is #{rank} ! Or maybe it's actually Paul that is #{rank}",
+        """
+        Reqvam is #{rank} ! And since I guess at least one of these messages should mention his twitch go give him a follow (as if somebody following this bot isn't already following him):
+
+        https://www.twitch.tv/reqvam
+        """,
+        "Reqvam is #{rank} ! Yes, the space before the ! is there so people can't make factorial jokes.",
+        "Reqvam is #{rank} ! *insert amusing comment* - coming up with these aint easy, if you have a suggestion fill this out: https://forms.gle/X6Wovae9aHGpvACZ7"
+      ]
 end
