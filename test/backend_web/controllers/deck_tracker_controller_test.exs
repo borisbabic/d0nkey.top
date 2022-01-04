46642cdb0b667c6c530882e7ac359c66f4e2e16e
Boris <boris.ivan.babic@gmail.com>
2022-01-05 00:41:14 +0100

    Add alpha message to streamer decks page

--- test/backend_web/controllers/deck_tracker_controller_test.exs
+++ test/backend_web/controllers/deck_tracker_controller_test.exs
@@ -22,6 +22,30 @@ defmodule BackendWeb.DeckTrackerControllerTest do
     format: 2,
     region: "KR"
   }
+
+  @valid_hdt_request %{
+    "Duration" => 9,
+    "Format" => 2,
+    "GameId" => "d287762e-3cf7-4741-a02c-29ae5fa3290e",
+    "GameType" => 1,
+    "Mode" => 7,
+    "Opponent" => %{
+      "Battletag" => "The Innkeeper",
+      "Class" => "Shaman",
+      "Deckcode" => nil,
+      "LegendRank" => 0,
+      "Rank" => 0
+    },
+    "Player" => %{
+      "Battletag" => "D0nkey#2470",
+      "Class" => "Priest",
+      "Deckcode" => "AAECAa0GHh6XAskGigf2B9MK65sD/KMDmakDn6kD8qwDha0DgbEDjrEDkbEDk7oDm7oDr7oDyL4DyMAD3swDlc0Dy80D184D49ED+9ED/tEDndgD4t4D+OMDAAA=",
+      "LegendRank" => 0,
+      "Rank" => 0
+    },
+    "Result" => "LOSS",
+    "Turns" => 0
+  }
   describe "put game" do
     @describetag :api_user
     test "400 when missing game_id", %{conn: conn} do
@@ -30,8 +54,19 @@ defmodule BackendWeb.DeckTrackerControllerTest do
     end
 
     test "create new game", %{conn: conn} do
-      conn = put(conn, Routes.deck_tracker_path(conn, :put_game), @valid_request)
+      game_id = Ecto.UUID.generate()
+      request = Map.put(@valid_request, :game_id, game_id)
+      conn = put(conn, Routes.deck_tracker_path(conn, :put_game), request)
       assert text_response(conn, 200) =~ "Success"
+      assert %{game_id: ^game_id} = Hearthstone.DeckTracker.get_game_by_game_id(game_id)
+    end
+
+    test "create hdt game", %{conn: conn} do
+      game_id = Ecto.UUID.generate()
+      request = Map.put(@valid_hdt_request, "GameId", game_id)
+      conn = put(conn, Routes.deck_tracker_path(conn, :put_game), request)
+      assert text_response(conn, 200) =~ "Success"
+      assert %{game_id: ^game_id} = Hearthstone.DeckTracker.get_game_by_game_id(game_id)
     end
   end
 end
