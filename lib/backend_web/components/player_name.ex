0c0f94f7500c2fa0d4ddcbdf595494c86c9c1d29
Boris <boris.ivan.babic@gmail.com>
2022-02-16 11:05:59 +0100

    Use PlayerName for group member names

--- lib/backend_web/components/player_name.ex
+++ lib/backend_web/components/player_name.ex
@@ -2,12 +2,11 @@ defmodule Components.PlayerName do
   @moduledoc """
   Render the player name, including player icon and optionally country flag
   """
-  use Surface.Component
-  use BackendWeb.ViewHelpers
+  use BackendWeb, :surface_component
 
   prop(player, :string)
   prop(text_link, :string, default: nil)
-  prop(flag, :boolean, default: false)
+  prop(flag, :boolean, default: true)
   prop(icon, :boolean, default: true)
   prop(link_class, :css_class, default: "")
 
@@ -16,11 +15,13 @@ defmodule Components.PlayerName do
     <span>
       <span :if={(country = country(@player)) && @flag}>{country_flag(country)}</span>
       <span :if={@icon}>{render_player_icon(@player)}</span>
-      <a :if={@text_link} class={@link_class} href={@text_link}>{@player}</a>
-      <span :if={! @text_link}>{@player}</span>
+      <a class={@link_class} href={text_link(@text_link, @player)}>{@player}</a>
     </span>
     """
   end
+
+  def text_link(nil, player), do: Routes.player_path(BackendWeb.Endpoint, :player_profile, player)
+  def text_link(link, _), do: link
 
   def country(player), do: Backend.PlayerInfo.get_country(player)
 end
