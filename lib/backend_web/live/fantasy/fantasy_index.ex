5fe80361b3bea7dfa469bde0ff4dad2ca7e62d67
Boris <boris.ivan.babic@gmail.com>
2022-03-13 03:30:33 +0100

    Update for MT#Two

--- lib/backend_web/live/fantasy/fantasy_index.ex
+++ lib/backend_web/live/fantasy/fantasy_index.ex
@@ -18,8 +18,7 @@ defmodule BackendWeb.FantasyIndexLive do
           <div class="level-left">
             <FantasyModal id="create_modal" title="Create Fantasy League"/>
             <a class="is-link button" href="/fantasy/leagues/join/7fa963f1-9d15-499b-9e62-03cba011980d">2022 GM Fantasy</a>
-            <a :if={show_mt?(:"Masters Tour One")} class="is-link button" href="/fantasy/leagues/join/b633b985-df60-4eca-a123-d4574c9ac51b">Onyxia's Lair (MT One)</a>
-            <a :if={show_mt?(:Undercity)} class="is-link button" href="/fantasy/leagues/join/fc8870ed-8e70-4260-822a-e52a3ca69562">Play Undercity Fantasy</a>
+            <a :if={show_mt?(:"Masters Tour Two")} class="is-link button" href="/fantasy/leagues/join/da778fb0-ee5c-4e97-b582-704a2434cfc7">Ruins of Alterac (MT Two)</a>
             <a :for={{tour, link} <- Dreamhack.current_fantasy()} class="is-link button" href={"#{link}"}>Join DH {tour}</a>
           </div>
         </div>
