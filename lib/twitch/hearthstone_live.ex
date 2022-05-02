635bafab8341d31927b8ac56e393f3bfd6d48713
Boris <boris.ivan.babic@gmail.com>
2022-05-02 22:17:55 +0200

    Add twitch streams for max open and btlfy

--- lib/twitch/hearthstone_live.ex
+++ lib/twitch/hearthstone_live.ex
@@ -33,4 +33,23 @@ defmodule Twitch.HearthstoneLive do
 
   def twitch_id_live?(nil), do: false
   def twitch_id_live?(twitch_id), do: streams() |> Enum.any?(&(to_string(&1.user_id) == to_string(twitch_id)))
+
+  def twitch_display_live?(twitch_display, title) when nil in [twitch_display, title], do: false
+  def twitch_display_live?(twitch_display, title_contains) when is_binary(title_contains) do
+    case get_by_twitch_display(twitch_display) do
+      %{title: title} -> String.contains?(title, title_contains)
+      _ -> false
+    end
+  end
+
+  @spec get_by_twitch_display(String.t()) :: Twitch.Stream.t() | nil
+  def get_by_twitch_display(twitch_display) do
+    Enum.find(streams(), & &1.user_name == twitch_display)
+  end
+
+  @spec get_by_twitch_login(String.t()) :: Twitch.Stream.t() | nil
+  def get_by_twitch_login(twitch_login) do
+    Enum.find(streams(), & twitch_login && Twitch.Stream.login(&1.titch_login) == twitch_login)
+  end
+
 end
