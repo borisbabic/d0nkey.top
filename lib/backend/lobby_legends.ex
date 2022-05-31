3dda6d7c0a20252ab2f96575b88d9dd0bce06a59
Boris Babic <boris.ivan.babic@gmail.com>
2022-05-31 12:48:29 +0200

    Fix lobby legends ladder

--- lib/backend/lobby_legends.ex
+++ lib/backend/lobby_legends.ex
@@ -1,9 +1,11 @@
 defmodule Backend.LobbyLegends do
-
+  @moduledoc false
   defmacro is_lobby_legends(season) do
+    ll = Enum.map(1..100, &"lobby_legends_#{&1}")
+    check = [5, 6 | ll]
+
     quote do
-      unquote(season) in [5, "lobby_legends_1", "lobby_legends_2", "lobby_legends_3"]
+      unquote(season) in unquote(check)
     end
   end
-
 end
