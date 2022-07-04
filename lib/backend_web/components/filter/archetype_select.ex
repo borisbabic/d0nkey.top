f1389b84a830636f5ffdb41ce9521ec6c416c64f
Boris Babic <boris.ivan.babic@gmail.com>
2022-07-04 03:55:07 +0200

    Add archetype dropdown to /decks

---
+++ lib/backend_web/components/filter/archetype_select.ex
@@ -0,0 +1,74 @@
+defmodule Components.Filter.ArchetypeSelect do
+  @moduledoc false
+  use Surface.LiveComponent
+  alias Components.Dropdown
+  alias Surface.Components.Form
+  alias Surface.Components.Form.TextInput
+  prop(update_fun, :fun)
+  prop(selected, :list, default: [])
+  prop(title, :string, default: "Select Archetype")
+  prop(search, :string, default: "")
+  prop(selectable_archetypes, :list, default: [])
+  prop(criteria, :list, default: [{"latest", 60 * 12}])
+
+  def render(assigns) do
+    ~F"""
+      <Dropdown title={"#{@title}"}>
+        <Form for={:search} change="search" submit="search" opts={autocomplete: "off"}>
+          <div class="columns is-mobile is-multiline">
+            <div class="column is-narrow">
+              <TextInput class="input" opts={placeholder: "Search"}/>
+            </div>
+          </div>
+        </Form>
+        <a class="dropdown-item is-active" :on-click="remove_archetype" :for={selected <- @selected} phx-value-archetype={selected}>
+          {selected}
+        </a>
+        <a class="dropdown-item" :for={archetype <- archetypes(@search, @selected, @selectable_archetypes, @criteria)} :on-click="add_archetype" phx-value-archetype={archetype}>
+          {archetype}
+        </a>
+      </Dropdown>
+
+    """
+  end
+
+  def handle_event("search", %{"search" => [search]}, socket),
+    do: {:noreply, assign(socket, :search, search)}
+
+  def handle_event(
+        "remove_archetype",
+        %{"archetype" => archetype},
+        socket = %{assigns: %{update_fun: update_fun, selected: selected}}
+      ) do
+    update_fun.(selected -- [archetype])
+    {:noreply, socket}
+  end
+
+  def handle_event(
+        "add_archetype",
+        %{"archetype" => archetype},
+        socket = %{assigns: %{update_fun: update_fun, selected: selected}}
+      ) do
+    update_fun.([archetype | selected])
+    {:noreply, socket}
+  end
+
+  def archetypes(search, selected, selectable_archetypes, criteria) do
+    num_to_show = (7 - Enum.count(selected)) |> max(3)
+
+    archetypes(selectable_archetypes, criteria)
+    |> Enum.filter(&(String.downcase(to_string(&1)) =~ String.downcase(search)))
+    |> Enum.reject(&(to_string(&1) in selected))
+    |> Enum.take(num_to_show)
+  end
+
+  defp archetypes([], criteria), do: Backend.Hearthstone.archetypes(criteria)
+  defp archetypes(selectable, _), do: selectable
+
+  def update_archetypes_fun(params, param, name \\ :update_params) do
+    fn val ->
+      new_params = Map.put(params, param, val)
+      Process.send_after(self(), {name, new_params}, 0)
+    end
+  end
+end
---
+++ test/backend_web/live/decks_live_test.exs
@@ -0,0 +1,121 @@
+defmodule BackendWeb.Live.DecksTest do
+  use BackendWeb.ConnCase
+  import Plug.Conn
+  import Phoenix.LiveViewTest
+  alias Hearthstone.DeckTracker.GameDto
+  alias Hearthstone.DeckTracker.PlayerDto
+
+  @priest_code "AAEBAa0GKB74B/oO1hGDuwKwvALRwQLZwQLfxAKQ0wLy7AKXhwPmiAO9mQPrmwP8owPIvgPDzAPXzgP70QPi3gP44wOb6wOf6wOm7wO79wO+nwSEowSLowTlsASJsgTHsgSktgSWtwTbuQTsyQSW1ASY1ASa1ASX7wQAAA=="
+  @warlock_code "AAEBAf0GKPoO2LsC870C38QCkMcC58sCrs0C8tACnPgC1IYDgIoD2psD/KMDnakD66wDvb4D184D9tYDxt4DzuED+OMDkuQDk+QDpu8D0PkDgfsDg/sDsJEEg6AEhaAE56AE26ME5bAEx7IE1bIE9ccE9c4EmNQEmtQEl+8EAAA="
+  @paladin_code "AAECAZ8FHvvoA8zrA5HsA6bvA/D2A4v4A8D5A9D5A7eABOCLBIuNBJyfBO6fBNCsBKWtBISwBLCyBJa3BNC9BNe9BLLBBLvOBJLUBJrUBKHUBPDxBLKeBZCkBZGkBZKkBQAA"
+  @highlander_priest %GameDto{
+    player: %PlayerDto{
+      battletag: "D0nkey#2470",
+      # Diamond 1
+      rank: 50,
+      legend_rank: nil,
+      deckcode: @priest_code,
+      class: "PRIEST"
+    },
+    opponent: %PlayerDto{
+      battletag: nil,
+      rank: nil,
+      legend_rank: nil,
+      deckcode: nil,
+      class: "WARLOCK"
+    },
+    game_id: "",
+    game_type: 7,
+    format: 1,
+    result: "WON"
+  }
+
+  @highlander_paladin %GameDto{
+    player: %PlayerDto{
+      battletag: "D0nkey#2470",
+      # Diamond 1
+      rank: 50,
+      legend_rank: nil,
+      deckcode: @paladin_code,
+      class: "PALADIN"
+    },
+    opponent: %PlayerDto{
+      battletag: nil,
+      rank: nil,
+      legend_rank: nil,
+      deckcode: nil,
+      class: "ROGUE"
+    },
+    game_id: "",
+    game_type: 7,
+    format: 2,
+    result: "WON"
+  }
+
+  @highlander_warlock %GameDto{
+    player: %PlayerDto{
+      battletag: "D0nkey#2470",
+      # Diamond 1
+      rank: 51,
+      legend_rank: 69,
+      deckcode: @warlock_code,
+      class: "WARLOCK"
+    },
+    opponent: %PlayerDto{
+      battletag: nil,
+      rank: nil,
+      legend_rank: nil,
+      deckcode: nil,
+      class: "WARRIOR"
+    },
+    game_id: "",
+    game_type: 7,
+    format: 1,
+    result: "WON"
+  }
+
+  setup_all do
+    deck_fixtures(@highlander_priest)
+    deck_fixtures(@highlander_warlock)
+  end
+
+  defp generate_btag() do
+    Ecto.UUID.generate() |> String.replace("-", "") |> Kernel.<>("#0000")
+  end
+
+  def deck_fixtures(base_dto, num \\ 101) do
+    1..num
+    |> Enum.map(fn _ ->
+      base_dto
+      |> Map.put(:game_id, Ecto.UUID.generate())
+      # We don't want the game to be marked as being the same
+      |> Map.update!(:opponent, fn o ->
+        Map.put(o, :battletag, generate_btag())
+      end)
+      |> Hearthstone.DeckTracker.handle_game()
+    end)
+  end
+
+  test "renders", %{conn: conn} do
+    {:ok, view, html} = live(conn, "/decks")
+    assert html =~ "Decks"
+  end
+
+  test "includes wild highlander priest and warlock", %{conn: conn} do
+    {:ok, view, html} = live(conn, "/decks?format=1")
+    assert html =~ @warlock_code
+    assert html =~ @priest_code
+  end
+
+  test "Legend excludes priest includes warlock decks", %{conn: conn} do
+    {:ok, view, html} = live(conn, "/decks?format=1&rank=legend")
+    refute html =~ @priest_code
+    assert html =~ @warlock_code
+  end
+
+  test "Archetype exludes warlcok and includes priest", %{conn: conn} do
+    {:ok, view, html} = live(conn, "/decks?format=1&archetype=Highlander Priest")
+    refute html =~ @warlock_code
+    assert html =~ @priest_code
+  end
+end
