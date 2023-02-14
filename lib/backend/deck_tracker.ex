8878afc988a5fb35dabf549ad082d939656aba3c
Boris <boris.ivan.babic@gmail.com>
2023-02-14 17:23:46 +0100

    Add miniset filter

--- lib/backend/deck_tracker.ex
+++ lib/backend/deck_tracker.ex
@@ -468,6 +468,11 @@ defmodule Hearthstone.DeckTracker do
     query |> where([game: g], g.inserted_at >= ^release)
   end
 
+  defp compose_games_query({"period", "naxxramas"}, query) do
+    release = ~N[2023-02-14 18:15:00]
+    query |> where([game: g], g.inserted_at >= ^release)
+  end
+
   defp compose_games_query(rank, query) when rank in [:legend, :diamond_to_legend],
     do: compose_games_query({"rank", to_string(rank)}, query)
 
