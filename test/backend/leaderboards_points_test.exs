83f2188391563b247d9a6a1015754fc00c0303ac
Boris Babic <boris.ivan.babic@gmail.com>
2023-03-28 09:30:06 +0200

    Add 2023 ldb points calculation

---
+++ test/backend/leaderboards_points_test.exs
@@ -0,0 +1,25 @@
+defmodule Backend.LeaderboardsPointsTest do
+  @moduledoc false
+  use Backend.DataCase
+  alias Backend.LeaderboardsPoints
+
+  test "TEST PERSON has 8 for january and 6 for february" do
+    for {s, rank} <- [{111, 1}, {112, 7}] do
+      season = %Hearthstone.Leaderboards.Season{season_id: s, region: "US", leaderboard_id: "STD"}
+
+      Backend.Leaderboards.create_entries(
+        [%{rank: rank, account_id: "TEST PERSON", rating: nil}],
+        season
+      )
+    end
+
+    points =
+      LeaderboardsPoints.calculate(%{"points_season" => "2023_spring", "leaderboard_id" => "STD"})
+
+    {"TEST PERSON", season_points, 14} = points |> Enum.find(&(elem(&1, 0) == "TEST PERSON"))
+    # January
+    assert {_, _, 8} = Enum.find(season_points, &(elem(&1, 0) == 111))
+    # February
+    assert {_, _, 6} = Enum.find(season_points, &(elem(&1, 0) == 112))
+  end
+end
