5cc2ee4acc62d7d11240c0f4aa01fddfc4e6fb4c
Boris Babic <boris.ivan.babic@gmail.com>
2023-04-20 02:26:12 +0200

    Update elixir to 1.13

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [ 
-    elixir_1_12
+    beam.packages.erlangR23.elixir_1_13
     docker-compose 
     nodejs-slim-14_x # for npm
     watchexec # for test_watch
