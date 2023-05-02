fc9f6c21d2f783a1087cb5919a8e527566e022b5
Boris <boris.ivan.babic@gmail.com>
2023-05-02 02:02:00 +0200

    Add auto deck archetype fixing

--- lib/backend/hearthstone/deck_bag.ex
+++ lib/backend/hearthstone/deck_bag.ex
@@ -1,6 +1,8 @@
 defmodule Backend.Hearthstone.DeckBag do
   @moduledoc false
   use GenServer
+  alias Backend.Hearthstone
+  alias Backend.Hearthstone.DeckArchetyper
 
   def start_link(default) do
     GenServer.start_link(__MODULE__, default, name: __MODULE__)
@@ -15,7 +17,24 @@ defmodule Backend.Hearthstone.DeckBag do
     with nil <- get_fresh(id) do
       update(id)
     end
+    |> tap(&check_archetype/1)
   end
+
+  defp check_archetype(deck) do
+    Task.start(fn ->
+      if needs_archetype_update?(deck) do
+        Hearthstone.recalculate_decks_archetypes([deck])
+        update(deck.id)
+      end
+    end)
+  end
+
+  defp needs_archetype_update?(%{archetype: archetype} = deck)
+       when is_binary(archetype) or is_atom(archetype) do
+    to_string(archetype) != to_string(DeckArchetyper.archetype(deck))
+  end
+
+  defp needs_archetype_update?(_), do: true
 
   def update(id) do
     with deck = %{id: ^id} <- Backend.Hearthstone.get_deck(id) do
