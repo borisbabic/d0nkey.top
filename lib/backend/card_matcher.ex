749721f5a612f0651289cb829ece66476cb4a36a
Boris Babic <boris.ivan.babic@gmail.com>
2023-05-29 01:51:07 +0200

    Use new algorithm for matching cards

--- lib/backend/card_matcher.ex
+++ lib/backend/card_matcher.ex
@@ -1,13 +1,15 @@
 defmodule Backend.CardMatcher do
   @moduledoc "Match cards based on name"
-  @min_jaro_distance 0.85
-  def match_name(cards, card_name, cutoff \\ @min_jaro_distance) do
+  @min_distance 0.85
+  def match_name(cards, card_name, cutoff \\ @min_distance) do
     Enum.flat_map(cards, fn card ->
       distance_orig = do_distance(card_name, card.name)
-      distance_normalized = do_distance(card_name, card.name, &normalize_card_name/1)
+      # distance_normalized = do_distance(card_name, card.name, &normalize_card_name/1)
 
-      piece_distance = piece_distance(card, card_name, cutoff)
-      distance = Enum.max([distance_orig, distance_normalized, piece_distance])
+      # piece_distance = piece_distance(card, card_name, cutoff)
+      # distance = Enum.max([distance_orig, distance_normalized, piece_distance])
+
+      distance = distance_orig
 
       if distance >= cutoff do
         [{distance, card}]
@@ -18,30 +20,40 @@ defmodule Backend.CardMatcher do
     |> Enum.sort_by(&elem(&1, 0), :desc)
   end
 
-  defp piece_distance(card, card_name, cutoff) do
-    piece_values =
-      String.split(card.name, " ")
-      |> Enum.filter(&(String.length(&1) > 2))
-      |> Enum.flat_map(fn piece ->
-        [
-          do_distance(piece, card_name),
-          do_distance(piece, card_name, &normalize_card_name/1)
-        ]
-      end)
+  # defp piece_distance(card, card_name, cutoff) do
+  #   piece_values =
+  #     String.split(card.name, " ")
+  #     |> Enum.filter(&(String.length(&1) > 2))
+  #     |> Enum.flat_map(fn piece ->
+  #       [
+  #         do_distance(piece, card_name),
+  #         do_distance(piece, card_name, &normalize_card_name/1)
+  #       ]
+  #     end)
 
-    case piece_values do
-      [_ | _] ->
-        piece_values
-        |> Enum.max()
-        |> Kernel.*(cutoff)
+  #   case piece_values do
+  #     [_ | _] ->
+  #       piece_values
+  #       |> Enum.max()
+  #       |> Kernel.*(cutoff)
 
-      _ ->
-        0
-    end
-  end
+  #     _ ->
+  #       0
+  #   end
+  # end
 
   defp do_distance(first, second, normalizer \\ &String.downcase/1) do
-    String.jaro_distance(normalizer.(first), normalizer.(second))
+    Akin.compare(normalizer.(first), normalizer.(second),
+      algorithms: ["overlap", "substring_double_metaphone"]
+    )
+    |> average()
+  end
+
+  defp average(result) do
+    {sum, count} =
+      Enum.reduce(result, {0, 0}, fn {_key, val}, {sum, count} -> {sum + val, count + 1} end)
+
+    sum / count
   end
 
   defp normalize_card_name(name) do
