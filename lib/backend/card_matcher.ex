2cc52dd7d3af324661da4434453a5934382c6356
Boris Babic <boris.ivan.babic@gmail.com>
2023-05-29 03:18:03 +0200

    Speed up card matching SIGNIFICANTLY

--- lib/backend/card_matcher.ex
+++ lib/backend/card_matcher.ex
@@ -2,65 +2,30 @@ defmodule Backend.CardMatcher do
   @moduledoc "Match cards based on name"
   @min_distance 0.85
   def match_name(cards, card_name, cutoff \\ @min_distance) do
-    Enum.flat_map(cards, fn card ->
-      distance_orig = do_distance(card_name, card.name)
-      # distance_normalized = do_distance(card_name, card.name, &normalize_card_name/1)
+    cards
+    |> prepared_match()
+    |> match_optimized(card_name, cutoff)
+  end
 
-      # piece_distance = piece_distance(card, card_name, cutoff)
-      # distance = Enum.max([distance_orig, distance_normalized, piece_distance])
+  def match_optimized(prepared_cards, card_name, cutoff \\ @min_distance) do
+    composed = Akin.Util.compose(card_name)
 
-      distance = distance_orig
+    # overlap is MUCH faster, if it's too low to hit the cutoff no point doing the more expensive algo
+    overlap_cutoff = 1 - 2 * (1 - cutoff)
 
-      if distance >= cutoff do
-        [{distance, card}]
+    Enum.flat_map(prepared_cards, fn {cached, card} ->
+      with overlap when overlap >= overlap_cutoff <- Akin.Overlap.compare(composed, cached, []),
+           sdm <- Akin.SubstringDoubleMetaphone.compare(composed, cached, []),
+           similarity when similarity >= cutoff <- (overlap + sdm) / 2 do
+        [{similarity, card}]
       else
-        []
+        _ -> []
       end
     end)
     |> Enum.sort_by(&elem(&1, 0), :desc)
   end
 
-  # defp piece_distance(card, card_name, cutoff) do
-  #   piece_values =
-  #     String.split(card.name, " ")
-  #     |> Enum.filter(&(String.length(&1) > 2))
-  #     |> Enum.flat_map(fn piece ->
-  #       [
-  #         do_distance(piece, card_name),
-  #         do_distance(piece, card_name, &normalize_card_name/1)
-  #       ]
-  #     end)
-
-  #   case piece_values do
-  #     [_ | _] ->
-  #       piece_values
-  #       |> Enum.max()
-  #       |> Kernel.*(cutoff)
-
-  #     _ ->
-  #       0
-  #   end
-  # end
-
-  defp do_distance(first, second, normalizer \\ &String.downcase/1) do
-    Akin.compare(normalizer.(first), normalizer.(second),
-      algorithms: ["overlap", "substring_double_metaphone"]
-    )
-    |> average()
-  end
-
-  defp average(result) do
-    {sum, count} =
-      Enum.reduce(result, {0, 0}, fn {_key, val}, {sum, count} -> {sum + val, count + 1} end)
-
-    sum / count
-  end
-
-  defp normalize_card_name(name) do
-    name
-    |> String.replace("(Rank 1)", "")
-    |> String.replace(~r/[,\-';:]/, "")
-    |> String.downcase()
-    |> String.trim()
+  def prepared_match(cards) do
+    Enum.map(cards, fn c -> {Akin.Util.compose(c.name), c} end)
   end
 end
