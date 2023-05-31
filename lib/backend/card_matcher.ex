079cb7a4d85401efca94ac26bcee1397d925dc35
Boris Babic <boris.ivan.babic@gmail.com>
2023-05-31 23:02:38 +0200

    Sanitize card matching

--- lib/backend/card_matcher.ex
+++ lib/backend/card_matcher.ex
@@ -3,19 +3,19 @@ defmodule Backend.CardMatcher do
   @min_distance 0.85
   def match_name(cards, card_name, cutoff \\ @min_distance) do
     cards
-    |> prepared_match()
+    |> prepare_for_match()
     |> match_optimized(card_name, cutoff)
   end
 
   def match_optimized(prepared_cards, card_name, cutoff \\ @min_distance) do
-    composed = Akin.Util.compose(card_name)
+    composed = prepare_for_match(card_name)
 
     # overlap is MUCH faster, if it's too low to hit the cutoff no point doing the more expensive algo
     overlap_cutoff = 1 - 2 * (1 - cutoff)
 
     Enum.flat_map(prepared_cards, fn {cached, card} ->
       with overlap when overlap >= overlap_cutoff <- Akin.Overlap.compare(composed, cached, []),
-           sdm <- Akin.SubstringDoubleMetaphone.compare(composed, cached, []),
+           sdm when sdm > 0 <- Akin.SubstringDoubleMetaphone.compare(composed, cached, []),
            similarity when similarity >= cutoff <- (overlap + sdm) / 2 do
         [{similarity, card}]
       else
@@ -25,7 +25,15 @@ defmodule Backend.CardMatcher do
     |> Enum.sort_by(&elem(&1, 0), :desc)
   end
 
-  def prepared_match(cards) do
-    Enum.map(cards, fn c -> {Akin.Util.compose(c.name), c} end)
+  def prepare_for_match(name) when is_binary(name) do
+    name
+    |> String.downcase()
+    |> String.normalize(:nfd)
+    |> String.replace(~r/[^a-z-\s]/u, "")
+    |> Akin.Util.compose()
+  end
+
+  def prepare_for_match(cards) do
+    Enum.map(cards, fn c -> {prepare_for_match(c.name), c} end)
   end
 end
