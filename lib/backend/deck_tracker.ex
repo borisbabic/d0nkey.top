536257f8b7cf4355abcba5137aac4b02dcb1fc8d
Boris Babic <boris.ivan.babic@gmail.com>
2023-07-06 20:21:51 +0200

    Add patch filter for 26.6.2

--- lib/backend/deck_tracker.ex
+++ lib/backend/deck_tracker.ex
@@ -539,6 +539,11 @@ defmodule Hearthstone.DeckTracker do
     query |> where([game: g], g.inserted_at >= ^release)
   end
 
+  defp compose_games_query({"period", "patch_26.6.2"}, query) do
+    release = ~N[2023-07-06 18:20:00]
+    query |> where([game: g], g.inserted_at >= ^release)
+  end
+
   defp compose_games_query(rank, query) when rank in [:legend, :diamond_to_legend],
     do: compose_games_query({"rank", to_string(rank)}, query)
 
