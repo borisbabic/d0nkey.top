2bb5d9fa63899d7c5f41dd47f0e7a56071357a70
Boris Babic <boris.ivan.babic@gmail.com>
2023-07-09 20:55:04 +0200

    Add !(all-)reveals command

--- lib/backend/infrastructure/blizzard_communicator.ex
+++ lib/backend/infrastructure/blizzard_communicator.ex
@@ -26,4 +26,36 @@ defmodule Backend.Infrastructure.BlizzardCommunicator do
 
   def create_link(region, leaderboard_id, season_id),
     do: "#{create_link(region, leaderboard_id, nil)}&seasonId=#{season_id}"
+
+  @type reveal_item :: %{
+          url: String.t(),
+          id: integer(),
+          image_url: String.t(),
+          reveal_time: NaiveDateTime.t()
+        }
+  def reveal_schedule() do
+    with {:ok, %{body: body}} <- HTTPoison.get("https://hearthstone.blizzard.com/en-us/cards") do
+      parse_reveal_schedule(body)
+    end
+  end
+
+  def parse_reveal_schedule(body) do
+    with {:ok, html} <- Floki.parse_document(body),
+         [hype] <- Floki.find(html, "#cardGalleryMount") |> Floki.attribute("hypemachine"),
+         {:ok, %{"cards" => cards}} <- Jason.decode(hype) do
+      for %{"hype_url" => url, "image_url" => image_url, "reveal_time" => time_raw, "id" => id} <-
+            cards,
+          {result, time} = NaiveDateTime.from_iso8601(time_raw),
+          result == :ok do
+        %{
+          url: url,
+          id: id,
+          image_url: image_url,
+          reveal_time: time
+        }
+      end
+    else
+      _ -> {:error, "Could not parse reveal schedule"}
+    end
+  end
 end
---
+++ lib/bot/reveal_message_handler.ex
@@ -0,0 +1,77 @@
+defmodule Bot.RevealMessageHandler do
+  @moduledoc "Handles messages related to hearthstone card reveals"
+  alias Backend.Infrastructure.BlizzardCommunicator, as: BlizzApi
+  import Bot.MessageHandlerUtil
+  alias Nostrum.Struct.Embed
+  alias Nostrum.Api
+
+  def handle_reveals(msg) do
+    response =
+      reveals()
+      |> filter_current(3)
+      |> create_response(msg)
+
+    Api.create_message(msg.channel_id, response)
+  end
+
+  def handle_all_reveals(msg) do
+    response =
+      reveals()
+      |> create_response(msg)
+
+    Api.create_message(msg, response)
+  end
+
+  def filter_current(reveals, minimum \\ 3) do
+    now = NaiveDateTime.utc_now()
+    start_period = NaiveDateTime.add(now, -1 * 60 * 60)
+    end_period = NaiveDateTime.add(now, 24 * 60 * 60)
+
+    after_start =
+      reveals |> Enum.drop_while(&(:gt == NaiveDateTime.compare(start_period, &1.reveal_time)))
+
+    min_reveals = Enum.take(after_start, minimum)
+
+    additional =
+      after_start
+      |> Enum.drop(minimum)
+      |> Enum.take_while(&(:gt == NaiveDateTime.compare(end_period, &1.reveal_time)))
+
+    min_reveals ++ additional
+  end
+
+  def create_response(reveals, msg) do
+    case format(msg) do
+      "embed" -> [embeds: Enum.map(reveals, &to_embed/1)]
+      _ -> [content: Enum.map_join(reveals, "\n", &to_text/1)]
+    end
+  end
+
+  def format(msg) do
+    msg
+    |> get_options()
+    |> Enum.find_value("embed", fn
+      "format:" <> format -> format
+      _ -> false
+    end)
+  end
+
+  def to_text(%{url: url, reveal_time: reveal_time}) do
+    {:ok, datetime} = DateTime.from_naive(reveal_time, "UTC")
+    timestamp = DateTime.to_unix(datetime)
+    "* <t:#{timestamp}> #{url}"
+  end
+
+  def to_embed(%{url: url, image_url: image_url, reveal_time: reveal_time}) do
+    title = if url == "", do: nil, else: "Reveal Link"
+
+    %Embed{
+      url: url,
+      title: title,
+      timestamp: reveal_time
+    }
+    |> Embed.put_image(image_url)
+  end
+
+  def reveals(), do: BlizzApi.reveal_schedule()
+end
