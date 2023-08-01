d913fbe17c91aa9297ffb2cbd877bdf0b23d286a
Boris Babic <boris.ivan.babic@gmail.com>
2023-08-01 02:32:24 +0200

    Add filter for titans

--- lib/backend/deck_tracker.ex
+++ lib/backend/deck_tracker.ex
@@ -656,6 +656,11 @@ defmodule Hearthstone.DeckTracker do
     query |> where([game: g], g.inserted_at >= ^release)
   end
 
+  defp compose_games_query({"period", "titans"}, query) do
+    release = ~N[2023-08-01 17:20:00]
+    query |> where([game: g], g.inserted_at >= ^release)
+  end
+
   defp compose_games_query(rank, query) when rank in [:legend, :diamond_to_legend],
     do: compose_games_query({"rank", to_string(rank)}, query)
 
