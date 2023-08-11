28d9d92b22ea93b43c7c9e4a76cf9430d5eb75dd
Boris Babic <boris.ivan.babic@gmail.com>
2023-08-11 23:55:33 +0200

    Ensure no duplicate errors on prefill

--- priv/repo/migrations/20230811153443_leaderboards_entry_current_prefill.exs
+++ priv/repo/migrations/20230811153443_leaderboards_entry_current_prefill.exs
@@ -4,10 +4,10 @@ defmodule Backend.Repo.Migrations.LeaderboardsEntryCurrentPrefill do
   def change do
     sql = """
       INSERT INTO public.leaderboards_current_entries (rank, account_id, rating, season_id, inserted_at)
-      SELECT DISTINCT le.rank, le.account_id, le.rating, le.season_id, le.inserted_at 
+      SELECT DISTINCT ON (le.rank, le.season_id) le.rank, le.account_id, le.rating, le.season_id, le.inserted_at 
       FROM public.leaderboards_entry le
       INNER JOIN public.leaderboards_entry_latest lat
-      ON lat.season_id = le.season_id AND lat.rank = le.rank AND lat.inserted_at = le.inserted_at;
+      ON lat.season_id = le.season_id AND lat.rank = le.rank AND lat.inserted_at = le.inserted_at
     """
 
     delete_sql = """
