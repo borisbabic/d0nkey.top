c443d18676fdc6132897e7305021b056a5b37177
Boris Babic <boris.ivan.babic@gmail.com>
2023-09-07 19:09:11 +0200

    Enable copying partial to ll bgs

--- lib/backend/hearthstone/deck_deduplicator.ex
+++ lib/backend/hearthstone/deck_deduplicator.ex
@@ -1,7 +1,11 @@
 defmodule Backend.Hearthstone.DeckDeduplicator do
   @moduledoc "Deduplicate decks with the same cards, format, hero and sideboards"
   use Oban.Worker, queue: :deck_deduplicator, unique: [period: 3_600]
+  import Ecto.Query
+
+  alias Hearthstone.DeckTracker
   alias Backend.Hearthstone
+  alias Backend.Hearthstone.Deck
   alias Backend.Repo
 
   def perform(%Oban.Job{args: %{"ids" => ids}}) do
@@ -21,5 +25,32 @@ defmodule Backend.Hearthstone.DeckDeduplicator do
       Oban.insert(multi, Jason.encode(arg), new(arg))
     end)
     |> Repo.transaction(timeout: 666_000)
+  end
+
+  def enqueue_played(num \\ 1000) when is_integer(num) do
+    [{"period", "past_2_weeks"}, {"min_games", 50}, {"limit", num}]
+    |> enqueue_played()
+  end
+
+  def enqueue_played(deck_stats_criteria) when is_list(deck_stats_criteria) do
+    deck_stats_criteria
+    |> DeckTracker.deck_stats()
+    |> Enum.map(& &1.deck_id)
+    |> enqueue_duplicates_for_deck_ids()
+  end
+
+  def enqueue_duplicates_for_deck_ids(deck_ids) when is_list(deck_ids) do
+    query =
+      from d in Deck,
+        join: d2 in Deck,
+        on:
+          d.cards == d2.cards and d.format == d2.format and d.hero == d2.hero and
+            d.sideboards == d2.sideboards and d.id != d2.id,
+        where: d.id in ^deck_ids,
+        select: %{ids: [d.id, d2.id]}
+
+    Repo.all(query)
+    |> Enum.uniq_by(&Enum.sort(&1.ids))
+    |> insert_all()
   end
 end
