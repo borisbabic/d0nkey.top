86b36446f37f2b6d2035f76e03531e23e0cadcb0
Boris <boris.ivan.babic@gmail.com>
2024-02-26 12:39:57 +0100

    Fix triangle helper

--- lib/backend_web/components/helper.ex
+++ lib/backend_web/components/helper.ex
@@ -2,7 +2,8 @@ defmodule Components.Helper do
   use Phoenix.Component
 
   def warning_triangle(), do: warning_triangle(%{})
-  attr :before, :any, required: false
+
+  attr :before, :any, required: false, default: false
 
   def warning_triangle(assigns) do
     ~H"""
