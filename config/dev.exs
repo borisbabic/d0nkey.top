cef8799869309f4a199caed7fe42b83bef7630b1
Boris <boris.ivan.babic@gmail.com>
2024-05-24 13:00:48 +0200

    Add esbuild

--- config/dev.exs
+++ config/dev.exs
@@ -43,13 +43,14 @@ config :backend, BackendWeb.Endpoint,
   code_reloader: true,
   check_origin: false,
   watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
-    ]
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    # node: [
+    #   "node_modules/webpack/bin/webpack.js",
+    #   "--mode",
+    #   "development",
+    #   "--watch-stdin",
+    #   cd: Path.expand("../assets", __DIR__)
+    # ]
   ]
 
 # ## SSL Support
