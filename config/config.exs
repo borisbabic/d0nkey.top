862a53fb158e959bba8ec359bdb7dd3aa39a86c7
Boris <boris.ivan.babic@gmail.com>
2024-06-23 04:39:47 +0200

    Fix esbuild js path

--- config/config.exs
+++ config/config.exs
@@ -160,7 +160,7 @@ config :esbuild,
   version: "0.20.2",
   default: [
     args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/js --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__)
   ]
 
