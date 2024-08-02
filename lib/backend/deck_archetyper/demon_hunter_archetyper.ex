038c7a2bb5ed6af7ded5c66c2b0bdfa17c28e3e7
tomka700 <125925723+tomka700@users.noreply.github.com>
2024-08-02 12:40:12 +0200

    Separate Fatigue Demon Hunter from Questline DH

--- lib/backend/deck_archetyper/demon_hunter_archetyper.ex
+++ lib/backend/deck_archetyper/demon_hunter_archetyper.ex
@@ -243,6 +243,9 @@ defmodule Backend.DeckArchetyper.DemonHunterArchetyper do
       fel_dh?(card_info) ->
         :"Fel DH"
 
+      wild_fatigue?(card_info) && questline?(card_info) ->
+        :"Fatigue Demon Hunter"
+
       questline?(card_info) ->
         :"Questline DH"
 
@@ -286,6 +289,13 @@ defmodule Backend.DeckArchetyper.DemonHunterArchetyper do
       "Sigil of Skydiving",
       "Hozen Roughhouser",
       "Dangerous Cliffside"
+    ])
+  end
+
+  def wild_fatigue?(card_info) do
+    min_count?(card_info, 2, [
+      "Aranna, Thrill Seeker",
+      "Glaivetar"
     ])
   end
 end
