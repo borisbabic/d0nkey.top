2ed2901ada33d84b2ab278b66e3bd42d27bdd324
Boris <boris.ivan.babic@gmail.com>
2024-08-14 11:55:51 +0200

    Improve DK archetyping

--- lib/backend/deck_archetyper/death_knight_archetyper.ex
+++ lib/backend/deck_archetyper/death_knight_archetyper.ex
@@ -15,13 +15,16 @@ defmodule Backend.DeckArchetyper.DeathKnightArchetyper do
       handbuff_dk?(card_info) ->
         :"Handbuff DK"
 
-      rainbow_dk?(card_info) && plague_dk?(card_info) ->
+      rainbow_runes?(card_info) && plague_dk?(card_info) ->
         :"Rainbow Plague DK"
 
-      rainbow_dk?(card_info) && excavate_dk?(card_info) ->
+      rainbow_runes?(card_info) && excavate_dk?(card_info) ->
         :"Rainbow Excavate DK"
 
-      rainbow_dk?(card_info) ->
+      buttons?(card_info) && rainbow_runes?(card_info) ->
+        :"Buttons Rainbow DK"
+
+      rainbow_runes?(card_info) ->
         :"Rainbow DK"
 
       plague_dk?(card_info) ->
@@ -48,12 +51,61 @@ defmodule Backend.DeckArchetyper.DeathKnightArchetyper do
       control_dk?(card_info) ->
         :"Control DK"
 
+      buttons?(card_info) ->
+        :"Buttons DK"
+
+      "Frostbitten Freebooter" in card_info.card_names and deathrattle?(card_info) ->
+        :"Frostbitten DK"
+
+      deathrattle?(card_info) ->
+        :"Deathrattle DK"
+
+      only_runes?(card_info, :blood) ->
+        :"Blood DK"
+
+      only_runes?(card_info, :frost) ->
+        :"Frost DK"
+
+      only_runes?(card_info, :unholy) ->
+        :"Unholy DK"
+
+      "Stitched Giant" in card_info.card_names ->
+        :"Corpse DK"
+
       true ->
         fallbacks(card_info, "DK", ignore_types: ["Undead", "undead", "UNDEAD"])
     end
   end
 
-  def rainbow_dk?(ci) do
+  defp buttons?(ci) do
+    "Buttons" in ci.card_names and
+      min_count?(ci, 2, [
+        "Razzle-Dazzler",
+        "Natural Talent",
+        "Malted Magma",
+        "Cabaret Headliner",
+        "Siren Song",
+        "Carress, Cabaret Star"
+      ])
+  end
+
+  defp deathrattle?(ci, min_count \\ 3) do
+    min_count?(ci, min_count, [
+      "Death Growl",
+      "Brittlebone Buccaneer",
+      "Dead Air",
+      "Yelling Yodeler"
+    ])
+  end
+
+  def only_runes?(ci, rune, min \\ 1) do
+    rune_cost = Deck.rune_cost(ci.cards) |> Map.from_struct()
+    {rune_count, others} = Map.pop(rune_cost, rune, 0)
+    others_zero? = Enum.all?(others, fn {_key, val} -> val == 0 end)
+    rune_count >= min and others_zero?
+  end
+
+  def rainbow_runes?(ci) do
     case Deck.rune_cost(ci.cards) do
       %{blood: b, frost: f, unholy: u} when b > 0 and f > 0 and u > 0 -> true
       _ -> false
