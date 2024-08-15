59891f7c1f947706456ca724181197be6a3722b4
Boris <boris.ivan.babic@gmail.com>
2024-08-15 21:09:44 +0200

    Remove old Hunter archetypes

--- lib/backend/deck_archetyper/hunter_archetyper.ex
+++ lib/backend/deck_archetyper/hunter_archetyper.ex
@@ -9,32 +9,11 @@ defmodule Backend.DeckArchetyper.HunterArchetyper do
       highlander?(card_info) ->
         :"Highlander Hunter"
 
-      quest?(card_info) || questline?(card_info) ->
-        :"Quest Hunter"
-
-      vanndar?(card_info) && big_beast_hunter?(card_info) ->
-        :"Vanndar Beast Hunter"
-
-      vanndar?(card_info) ->
-        :"Vanndar Hunter"
-
-      arcane_hunter?(card_info) && (big_beast_hunter?(card_info) or beast_hunter?(card_info)) ->
-        :"Arcane Beast Hunter"
-
-      arcane_hunter?(card_info) ->
-        :"Arcane Hunter"
-
       secret_hunter?(card_info) ->
         :"Secret Hunter"
 
-      rat_hunter?(card_info) ->
-        :"Rattata Hunter"
-
       big_beast_hunter?(card_info) ->
         :"Big Beast Hunter"
-
-      cleave_hunter?(card_info) ->
-        :"Cleave Hunter"
 
       handbuff_hunter?(card_info) ->
         :"Handbuff Hunter"
@@ -51,26 +30,14 @@ defmodule Backend.DeckArchetyper.HunterArchetyper do
       murloc?(card_info) ->
         :"Murloc Hunter"
 
-      boar?(card_info) ->
-        :"Boar Hunter"
-
       menagerie?(card_info) ->
         :"Menagerie Hunter"
-
-      aggro_hunter?(card_info) ->
-        :"Aggro Hunter"
-
-      shockspitter?(card_info) ->
-        :"Shockspitter Hunter"
 
       egg_hunter?(card_info) ->
         :"Egg Hunter"
 
       mystery_egg_hunter?(card_info) ->
         :"Mystery Egg Hunter"
-
-      wildseed_hunter?(card_info) ->
-        :"Wildseed Hunter"
 
       true ->
         fallbacks(card_info, "Hunter")
@@ -116,9 +83,7 @@ defmodule Backend.DeckArchetyper.HunterArchetyper do
   defp zoo_hunter?(ci) do
     min_count?(ci, 4, [
       "Observer of Myths",
-      "Hawkstrider Rancher",
       "Saddle Up!",
-      "Shadehound",
       "R.C. Rampage",
       "Remote Control",
       "Jungle Gym"
@@ -138,10 +103,6 @@ defmodule Backend.DeckArchetyper.HunterArchetyper do
         "Product 9",
         "Starstrung Bow"
       ])
-
-  def shockspitter?(ci) do
-    "Shockspitter" in ci.card_names
-  end
 
   def wild(card_info) do
     class_name = Deck.class_name(card_info.deck)
@@ -190,32 +151,6 @@ defmodule Backend.DeckArchetyper.HunterArchetyper do
     min_count?(card_info, 1, ["Mystery Egg"])
   end
 
-  defp cleave_hunter?(card_info) do
-    min_count?(card_info, 3, ["Hollow Hound", "Stonebound Gargon", "Always a Bigger Jormungar"]) &&
-      min_count?(card_info, 2, [
-        "Absorbent Parasite",
-        "Beastial Madness",
-        "Messenger Buzzard",
-        "Hope of Quel'Thalas"
-      ])
-  end
-
-  defp arcane_hunter?(card_info),
-    do:
-      min_count?(card_info, 2, ["Halduron Brightwing", "Silvermoon Farstrider", "Arcane Quiver"]) &&
-        min_spell_school_count?(card_info, 4, "arcane")
-
-  defp rat_hunter?(ci),
-    do:
-      min_count?(ci, 4, [
-        "Leartherworking Kit",
-        "Rodent Nest",
-        "Sin'dorei Scentfinder",
-        "Defias Blastfisher",
-        "Shadehound",
-        "Rats of Extraordinary Size"
-      ])
-
   defp big_beast_hunter?(ci),
     do:
       min_count?(ci, 3, [
@@ -227,18 +162,4 @@ defmodule Backend.DeckArchetyper.HunterArchetyper do
         "Thunderbringer",
         "Mister Mukla"
       ])
-
-  defp aggro_hunter?(ci),
-    do:
-      min_count?(ci, 2, [
-        "Doggie Biscuit",
-        "Bunch of Bananas",
-        "Vicious Slitherspear",
-        "Ancient Krakenbane",
-        "Arrow Smith",
-        "Raj Naz'jan"
-      ])
-
-  def wildseed_hunter?(ci),
-    do: min_count?(ci, 3, ["Spirit Poacher", "Stag Charge", "Wild Spirits", "Ara'lon"])
 end
