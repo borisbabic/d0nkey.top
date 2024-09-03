dfa5a098b0014e1a77b403fd0c4e700780b35ea7
Boris <boris.ivan.babic@gmail.com>
2024-09-03 15:28:50 +0200

    Shrink card images in cards explorer

--- lib/backend_web/components/card.ex
+++ lib/backend_web/components/card.ex
@@ -4,12 +4,13 @@ defmodule Components.Card do
 
   alias Backend.Hearthstone.Card
   prop(card, :map, required: true)
+  prop(shrink_mobile, :boolean, default: false)
   prop(disable_link, :boolean, default: false)
 
   def render(assigns) do
     ~F"""
       <a href={~p"/card/#{@card}"} class={"has-no-pointer-events": @disable_link}>
-        <image src={Card.card_url(@card)} alt={@card.name} width="256"/>
+        <img src={Card.card_url(@card)} alt={@card.name} class={"tw-w-80": !@shrink_mobile, "tw-w-48": @shrink_mobile, "lg:tw-w-80": @shrink_mobile, "md:tw-w-60": @shrink_mobile}/>
       </a>
     """
   end
