07621d0418ee1a089b69c9654d97afae66d24109
Boris <boris.ivan.babic@gmail.com>
2024-09-23 01:37:33 +0200

    Fix alpine data getting overrided

--- assets/js/app.js
+++ assets/js/app.js
@@ -181,7 +181,9 @@ let liveSocket = new LiveSocket("/live", Socket, {
     params: {_csrf_token: csrfToken},
     dom: {
         onBeforeElUpdated(from, to){
-          if(from.__x){ window.Alpine.clone(from.__x, to) }
+          if(from._x_dataStack){
+            window.Alpine.clone(from, to) ;
+          }
         }
     }
 })
