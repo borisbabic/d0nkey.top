719a2be9432e8d5df0ec8619ab9abe8366f25695
Boris <boris.ivan.babic@gmail.com>
2024-11-19 11:06:21 +0100

    Update TMI

--- lib/twitch_bot/handler.ex
+++ lib/twitch_bot/handler.ex
@@ -1,22 +1,24 @@
 defmodule TwitchBot.Handler do
   import TwitchBot.Util
-  use TMI.Handler
+  use TMI
+
   @type message_info :: %{
-    message: String.t(),
-    sender: String.t(),
-    chat: String.t(),
-  }
-  @impl true
+          message: String.t(),
+          sender: String.t(),
+          chat: String.t()
+        }
+  @impl TMI.Handler
   def handle_message(message, sender, chat) do
     config = chat |> parse_chat() |> Backend.TwitchBot.commands()
+
     message_info = %{
       message: message,
       sender: sender,
-      chat: chat,
+      chat: chat
     }
 
     matching = TwitchBot.MessageMatcher.match(config, message_info)
     responses = TwitchBot.MessageCreator.create_messages(matching, message_info)
-    Enum.each(responses, & TMI.message(chat, &1))
+    Enum.each(responses, &TMI.message(chat, &1))
   end
 end
