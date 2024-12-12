3d4a0eceb3ee9b2894616b73103eb16f5d1a4038
Boris <boris.ivan.babic@gmail.com>
2024-12-12 08:40:59 +0100

    Reneable oban and cron except leaderboards

--- config/config_bot.exs
+++ config/config_bot.exs
@@ -1,127 +1,121 @@
 import Config
 
-# config :backend, QuantumScheduler,
-#   jobs: [
-#     {"37 3 * * *", fn -> Backend.HearthstoneJson.update_cards() end},
-#     {"37 13 * * *", fn -> Backend.ReqvamTop100Tweeter.check_and_tweet() end},
-#     {"37 5 * * *", fn -> Backend.PrioritizedBattletagCache.update_cache() end},
-#     # {"1 * * * *", fn -> Backend.MastersTour.sign_me_up() end},
-#     {"17 * * * *", fn -> Backend.DeckFeedItemUpdater.update_deck_items() end},
-#     {"47 * * * *", fn -> Backend.Feed.decay_feed_items() end},
-#     # {"* * * * *", fn -> Backend.Grandmasters.update() end},
-#     {"53 * * * *", fn -> Backend.PlayerIconBag.update() end},
-#     # {"* * * * *", fn -> Backend.Streaming.update_hdt_streamer_decks() end},
-#     {"57 * * * *", fn -> Backend.MastersTour.refresh_current_invited() end},
+config :backend, QuantumScheduler,
+  jobs: [
+    {"37 3 * * *", fn -> Backend.HearthstoneJson.update_cards() end},
+    {"37 13 * * *", fn -> Backend.ReqvamTop100Tweeter.check_and_tweet() end},
+    {"37 5 * * *", fn -> Backend.PrioritizedBattletagCache.update_cache() end},
+    # {"1 * * * *", fn -> Backend.MastersTour.sign_me_up() end},
+    {"17 * * * *", fn -> Backend.DeckFeedItemUpdater.update_deck_items() end},
+    {"47 * * * *", fn -> Backend.Feed.decay_feed_items() end},
+    # {"* * * * *", fn -> Backend.Grandmasters.update() end},
+    {"53 * * * *", fn -> Backend.PlayerIconBag.update() end},
+    # {"* * * * *", fn -> Backend.Streaming.update_hdt_streamer_decks() end},
+    {"57 * * * *", fn -> Backend.MastersTour.refresh_current_invited() end},
 
-#     # {"41 * * * *", fn -> Backend.PonyDojo.update() end},
-#     {"43 * * * *", fn -> Backend.DiscordBot.update_all_guilds(5000) end},
-#     {"*/2 * * * *",
-#      fn ->
-#        Backend.Leaderboards.save_current_with_delay(
-#          [:CN],
-#          [:STD, :WLD, :twist],
-#          100,
-#          10_000,
-#          100
-#        )
-#      end},
-#     {"*/10 * * * *",
-#      fn ->
-#        Backend.Leaderboards.save_current_with_delay(
-#          [:CN],
-#          [:arena, :BG, :DUO],
-#          500,
-#          10_000,
-#          100
-#        )
-#      end},
-#     {"*/2 * * * *",
-#      fn ->
-#        Backend.Leaderboards.save_current_with_delay(
-#          [:EU, :US, :AP],
-#          [:STD, :WLD, :twist],
-#          50,
-#          10_000,
-#          1000
-#        )
-#      end},
-#     {"*/10 * * * *",
-#      fn ->
-#        Backend.Leaderboards.save_current_with_delay(
-#          [:EU, :US, :AP],
-#          [:BG, :DUO],
-#          100,
-#          10_000,
-#          1000
-#        )
-#      end},
-#     {"*/30 * * * *",
-#      fn ->
-#        Backend.Leaderboards.save_current_with_delay(
-#          [:EU, :US, :AP],
-#          [:arena],
-#          500,
-#          5000,
-#          1000
-#        )
-#      end},
-#     {"3 * * * *",
-#      fn ->
-#        Backend.Leaderboards.save_current_with_delay(
-#          [:EU, :US, :AP],
-#          [:STD, :WLD, :twist],
-#          200,
-#          5000,
-#          9000,
-#          1000
-#        )
-#      end},
-#     {"11 */2 * * *",
-#      fn ->
-#        Backend.Leaderboards.save_current_with_delay(
-#          [:EU, :US, :AP],
-#          [:BG, :DUO],
-#          200,
-#          10_000,
-#          9000,
-#          1000
-#        )
-#      end},
-#     {"11 */5 * * *",
-#      fn ->
-#        Backend.Leaderboards.save_current_with_delay(
-#          [:EU, :US, :AP],
-#          [:arena],
-#          1000,
-#          20_000,
-#          9000,
-#          1000
-#        )
-#      end},
-#     {"47 * * * *",
-#      fn ->
-#        Backend.Leaderboards.save_all_right_after_midnight(
-#          [:STD, :BG, :WLD, :twist, :DUO, :arena],
-#          5000,
-#          120_000,
-#          10_001
-#        )
-#      end},
-#     {"31 17 * * *", fn -> Backend.Hearthstone.update_metadata() end},
-#     {"* * * * *", fn -> Backend.Hearthstone.CardBag.refresh_table() end},
-#     {"*/15 * * * *", fn -> Backend.Hearthstone.CardUpdater.enqueue_latest_set() end},
-#     {"11 */2 * * *", fn -> Backend.Hearthstone.CardUpdater.enqueue_collectible() end},
-#     {"17 08 * * *", fn -> Backend.Hearthstone.enqueue_all() end},
-#     # {"*/9 * * * *", fn -> Backend.Leaderboards.refresh_latest() end},
-#     {"7 * * * *", fn -> Backend.Leaderboards.prune_empty_seasons() end},
-#     {"11 08 1 * *", fn -> Backend.Leaderboards.copy_last_month_to_lobby_legends() end},
-#     {"*/53 * * * *", fn -> Backend.Hearthstone.regenerate_false_neutral_deckcodes() end},
-#     {"* */2 * * *", fn -> Hearthstone.DeckTracker.ArchetypeBag.update() end},
-#     {"* * * * *", fn -> Hearthstone.DeckTracker.refresh_agg_stats() end},
-#     {"* * * * *", fn -> Backend.Patreon.add_new_tiers() end},
-#     {"13 * * * *", fn -> Backend.UserManager.update_patreon_tiers() end},
-#     {"*/4 * * * *", fn -> Backend.LatestHSArticles.update() end}
-#   ]
+    # {"41 * * * *", fn -> Backend.PonyDojo.update() end},
+    {"43 * * * *", fn -> Backend.DiscordBot.update_all_guilds(5000) end},
+    # {"*/2 * * * *",
+    #  fn ->
+    #    Backend.Leaderboards.save_current_with_delay( [:CN], [:STD, :WLD, :twist], 100, 10_000, 100)
+    #  end},
+    #     {"*/10 * * * *",
+    #      fn ->
+    #        Backend.Leaderboards.save_current_with_delay(
+    #          [:CN],
+    #          [:arena, :BG, :DUO],
+    #          500,
+    #          10_000,
+    #          100
+    #        )
+    #      end},
+    #     {"*/2 * * * *",
+    #      fn ->
+    #        Backend.Leaderboards.save_current_with_delay(
+    #          [:EU, :US, :AP],
+    #          [:STD, :WLD, :twist],
+    #          50,
+    #          10_000,
+    #          1000
+    #        )
+    #      end},
+    #     {"*/10 * * * *",
+    #      fn ->
+    #        Backend.Leaderboards.save_current_with_delay(
+    #          [:EU, :US, :AP],
+    #          [:BG, :DUO],
+    #          100,
+    #          10_000,
+    #          1000
+    #        )
+    #      end},
+    #     {"*/30 * * * *",
+    #      fn ->
+    #        Backend.Leaderboards.save_current_with_delay(
+    #          [:EU, :US, :AP],
+    #          [:arena],
+    #          500,
+    #          5000,
+    #          1000
+    #        )
+    #      end},
+    #     {"3 * * * *",
+    #      fn ->
+    #        Backend.Leaderboards.save_current_with_delay(
+    #          [:EU, :US, :AP],
+    #          [:STD, :WLD, :twist],
+    #          200,
+    #          5000,
+    #          9000,
+    #          1000
+    #        )
+    #      end},
+    #     {"11 */2 * * *",
+    #      fn ->
+    #        Backend.Leaderboards.save_current_with_delay(
+    #          [:EU, :US, :AP],
+    #          [:BG, :DUO],
+    #          200,
+    #          10_000,
+    #          9000,
+    #          1000
+    #        )
+    #      end},
+    #     {"11 */5 * * *",
+    #      fn ->
+    #        Backend.Leaderboards.save_current_with_delay(
+    #          [:EU, :US, :AP],
+    #          [:arena],
+    #          1000,
+    #          20_000,
+    #          9000,
+    #          1000
+    #        )
+    #      end},
+    #     {"47 * * * *",
+    #      fn ->
+    #        Backend.Leaderboards.save_all_right_after_midnight(
+    #          [:STD, :BG, :WLD, :twist, :DUO, :arena],
+    #          5000,
+    #          120_000,
+    #          10_001
+    #        )
+    #      end},
+    # {"7 * * * *", fn -> Backend.Leaderboards.prune_empty_seasons() end},
+    # {"11 08 1 * *", fn -> Backend.Leaderboards.copy_last_month_to_lobby_legends() end},
+    {"31 17 * * *", fn -> Backend.Hearthstone.update_metadata() end},
+    {"* * * * *", fn -> Backend.Hearthstone.CardBag.refresh_table() end},
+    {"*/15 * * * *", fn -> Backend.Hearthstone.CardUpdater.enqueue_latest_set() end},
+    {"11 */2 * * *", fn -> Backend.Hearthstone.CardUpdater.enqueue_collectible() end},
+    {"17 08 * * *", fn -> Backend.Hearthstone.enqueue_all() end},
+    # {"*/9 * * * *", fn -> Backend.Leaderboards.refresh_latest() end},
+    {"*/53 * * * *", fn -> Backend.Hearthstone.regenerate_false_neutral_deckcodes() end},
+    {"* */2 * * *", fn -> Hearthstone.DeckTracker.ArchetypeBag.update() end},
+    {"* * * * *", fn -> Hearthstone.DeckTracker.refresh_agg_stats() end},
+    {"* * * * *", fn -> Backend.Patreon.add_new_tiers() end},
+    {"13 * * * *", fn -> Backend.UserManager.update_patreon_tiers() end},
+    {"*/4 * * * *", fn -> Backend.LatestHSArticles.update() end}
+  ]
 
 max_queue_size =
   with raw when is_binary(raw) <- System.get_env("POOL_SIZE"),
