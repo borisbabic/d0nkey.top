9ccec5f7ba73e062799f4e12510bf76512e8b623
Boris <boris.ivan.babic@gmail.com>
2025-02-13 05:36:57 +0100

    Make cards smaller

--- lib/backend_web/components/card.ex
+++ lib/backend_web/components/card.ex
@@ -11,7 +11,7 @@ defmodule Components.Card do
   def render(assigns) do
     ~F"""
       <a href={~p"/card/#{@card}"} class={"tw-relative", "has-no-pointer-events": @disable_link}>
-        <img src={Card.card_url(@card)} alt={@card.name} class={"tw-w-80": !@shrink_mobile, "tw-w-48": @shrink_mobile, "lg:tw-w-80": @shrink_mobile, "md:tw-w-60": @shrink_mobile}/>
+        <img src={Card.card_url(@card)} alt={@card.name} class={"tw-w-64": !@shrink_mobile, "tw-w-48": @shrink_mobile, "lg:tw-w-64": @shrink_mobile, "md:tw-w-64": @shrink_mobile}/>
         <div class="tw-inline-block tw-text-center tw-absolute tw-top-0 tw-z-1 has-text-white tw-w-full">
           <#slot {@above_image, card: @card} />
         </div>
