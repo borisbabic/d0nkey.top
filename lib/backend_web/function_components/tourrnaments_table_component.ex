365b8868d8b41425894e7863a5abb1e33c1d320c
Boris <boris.ivan.babic@gmail.com>
2025-03-24 04:10:11 +0100

    Make already started tourneys distinct

--- lib/backend_web/function_components/tourrnaments_table_component.ex
+++ lib/backend_web/function_components/tourrnaments_table_component.ex
@@ -1,4 +1,5 @@
 defmodule FunctionComponents.TournamentsTable do
+  @moduledoc false
   use Phoenix.Component
   alias Backend.Tournaments.Tournament
   alias Components.Helper
@@ -24,9 +25,10 @@ defmodule FunctionComponents.TournamentsTable do
                 <%= Tournament.name(tournament) %>
               </a>
             </td>
-            <td>
-              <Helper.datetime datetime={Tournament.start_time(tournament)} />
+            <td :if={start_time = Tournament.start_time(tournament)} class={start_time_class(start_time)}>
+              <Helper.datetime datetime={start_time} />
             </td>
+            <td :if={!Tournament.start_time(tournament)}> </td>
             <td>
               <EsportsBadges.badges badges={Tournament.tags(tournament)} />
             </td>
@@ -36,5 +38,17 @@ defmodule FunctionComponents.TournamentsTable do
 
     </table>
     """
+  end
+
+  @spec start_time_class(NaiveDateTime.t()) :: boolean
+  defp start_time_class(start_time) do
+    if already_started?(start_time) do
+      "tw-text-gray-400"
+    end
+  end
+
+  @spec already_started?(NaiveDateTime.t()) :: boolean()
+  defp already_started?(start_time) do
+    :lt == NaiveDateTime.compare(start_time, NaiveDateTime.utc_now())
   end
 end
