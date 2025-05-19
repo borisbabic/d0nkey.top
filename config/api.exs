72b02d4b9e439b2133f4f60e546e6cf2265985d3
Boris <boris.ivan.babic@gmail.com>
2025-05-19 17:54:21 +0200

    Add api config

---
+++ config/api.exs
@@ -0,0 +1,28 @@
+import Config
+
+import_config "config_bot.exs"
+
+config :backend,
+  warmup_cache: true,
+  enable_bot: false,
+  auto_migrate: false,
+  thl_discord_id: 534_455_756_129_435_649,
+  nostrum_slash_command_target: :global,
+  dt_insert_listener: false
+
+# config :nostrum,
+# The token of your bot as a string
+# token: System.get_env("DISCORD_TOKEN"),
+# The number of shards you want to run your bot under, or :auto.
+# num_shards: :auto
+
+import_config "prod.secret.exs"
+
+# config :backend, Backend.Repo,
+# username: "root",
+# password: "root",
+# database: "DtopDB",
+# hostname: "localhost",
+# port: 2470,
+# show_sensitive_data_on_connection_error: true,
+# pool_size: 10
