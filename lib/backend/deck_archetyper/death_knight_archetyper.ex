444fcc97999f2b58462aa24a9c2a352b3d7ee408
Fülöp Tamás <125925723+tomka700@users.noreply.github.com>
2025-06-12 09:13:33 +0200

    Replace #{class_name} with DK
    Without touching the fallbacks part to avoid the issue from last time

--- lib/backend/deck_archetyper/death_knight_archetyper.ex
+++ lib/backend/deck_archetyper/death_knight_archetyper.ex
@@ -164,28 +164,25 @@ defmodule Backend.DeckArchetyper.DeathKnightArchetyper do
         :"Reno Rainbow DK"
 
       highlander?(card_info) ->
-        String.to_atom("Highlander #{class_name}")
-
-      questline?(card_info) ->
-        String.to_atom("Questline #{class_name}")
+        :"Reno DK"
 
       quest?(card_info) ->
-        String.to_atom("#{quest_abbreviation(card_info)} Quest #{class_name}")
+        String.to_atom("#{quest_abbreviation(card_info)} Quest DK")
 
       boar?(card_info) ->
-        String.to_atom("Boar #{class_name}")
+        :"Boar DK"
 
       baku?(card_info) ->
-        String.to_atom("Odd #{class_name}")
+        :"Odd DK"
 
       genn?(card_info) ->
-        String.to_atom("Even #{class_name}")
+        :"Even DK"
 
       "King Togwaggle" in card_info.card_names ->
-        String.to_atom("Tog #{class_name}")
+        :"Tog DK"
 
       "Mecha'thun" in card_info.card_names ->
-        "Mecha'thun #{class_name}"
+        :"Mecha'thun DK"
 
       buttons?(card_info) ->
         :"Buttons DK"
