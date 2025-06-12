2dff7864f2a9351924630edf1fd099d0a69dd4c8
Fülöp Tamás <125925723+tomka700@users.noreply.github.com>
2025-06-12 10:09:21 +0200

    Replace #{class_name} with Druid and rename HL to Reno

--- lib/backend/deck_archetyper/druid_archetyper.ex
+++ lib/backend/deck_archetyper/druid_archetyper.ex
@@ -116,25 +116,25 @@ defmodule Backend.DeckArchetyper.DruidArchetyper do
 
     cond do
       highlander?(card_info) ->
-        String.to_atom("Highlander #{class_name}")
+        :"Reno Druid"
 
       questline?(card_info) ->
-        String.to_atom("Questline #{class_name}")
+        :"Questline Druid"
 
       quest?(card_info) ->
-        String.to_atom("#{quest_abbreviation(card_info)} Quest #{class_name}")
+        String.to_atom("#{quest_abbreviation(card_info)} Quest Druid")
 
       boar?(card_info) ->
-        String.to_atom("Boar #{class_name}")
+        :"Boar Druid"
 
       baku?(card_info) ->
-        String.to_atom("Odd #{class_name}")
+        :"Odd Druid"
 
       genn?(card_info) ->
-        String.to_atom("Even #{class_name}")
+        :"Even Druid"
 
       "King Togwaggle" in card_info.card_names ->
-        String.to_atom("Tog #{class_name}")
+        :"Tog Druid"
 
       "Linecracker" in card_info.card_names ->
         :"Linecracker Druid"
