4cfe5745540f31c256aec8cf9beb4ac49decdfdf
Boris <boris.ivan.babic@gmail.com>
2025-07-03 16:08:21 +0200

    Make old migrations work in fresh db

--- priv/repo/migrations/20210309220544_broadcast_league_mutation.exs
+++ priv/repo/migrations/20210309220544_broadcast_league_mutation.exs
@@ -1,3 +1,8 @@
 defmodule Backend.Repo.Migrations.BroadcastLeagueMutation do
   # use PostgrexPubsub.BroadcastIdMigration, table_name: "leagues"
+  use Ecto.Migration
+  def up do
+  end
+  def down do
+  end
 end
--- priv/repo/migrations/20240824161836_init_canonical_deckcode_id_to_card.exs
+++ priv/repo/migrations/20240824161836_init_canonical_deckcode_id_to_card.exs
@@ -2,7 +2,7 @@ defmodule Backend.Repo.Migrations.InitCanonicalDeckcodeIdToCard do
   use Ecto.Migration
 
   def up do
-    Backend.Hearthstone.set_referent_card_ids()
+    #Backend.Hearthstone.set_referent_card_ids()
   end
 
   def down do
