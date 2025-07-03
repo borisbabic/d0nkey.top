d019ccf804ebf3ebb06dfd37188c1a4f4f4348ce
Boris <boris.ivan.babic@gmail.com>
2025-07-03 16:08:21 +0200

    Make old migrations work in fresh db

--- priv/repo/migrations/20210913135128_broadcast_dt_games_mutation.exs
+++ priv/repo/migrations/20210913135128_broadcast_dt_games_mutation.exs
@@ -1,3 +1,8 @@
 defmodule Backend.Repo.Migrations.BroadcastDtGamesMutation do
   # use PostgrexPubsub.BroadcastIdMigration, table_name: "dt_games"
+  use Ecto.Migration
+  def up do
+  end
+  def down do
+  end
 end
--- priv/repo/migrations/20240120211554_remove_games_broadcast.exs
+++ priv/repo/migrations/20240120211554_remove_games_broadcast.exs
@@ -2,7 +2,7 @@ defmodule Backend.Repo.Migrations.RemoveGamesBroadcast do
   use Ecto.Migration
 
   def up do
-    execute("DROP TRIGGER notify_dt_games_id_changes_trigger ON dt_games")
+    execute("DROP TRIGGER IF EXISTS notify_dt_games_id_changes_trigger ON dt_games")
   end
 
   def down do
