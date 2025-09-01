cfcd9eda35e0031d6929090827d0511e992e0fa5
Boris <boris.ivan.babic@gmail.com>
2025-09-01 09:16:38 +0200

    Test warlock archetyper update

--- lib/backend/played_cards_archetyper/warlock_archetyper.ex
+++ lib/backend/played_cards_archetyper/warlock_archetyper.ex
@@ -9,20 +9,20 @@ defmodule Backend.PlayedCardsArchetyper.WarlockArchetyper do
       quest?(card_info) ->
         :"Quest Warlock"
 
-      any?(card_info, [
-        "Hologram Operator",
-        "Questing Assistant",
-        "Sweetened Snowflurry",
-        "Spelunker",
-        "Tidepool Pupil",
-        "Mass Production",
-        "The Solarium",
-        "Clumsy Steward",
-        "Bloodpetal Biome",
-        "Sketch Artist",
-        "Tunnel Terror"
-      ]) ->
-        :"Quest Warlock"
+      # any?(card_info, [
+      #   "Hologram Operator",
+      #   "Questing Assistant",
+      #   "Sweetened Snowflurry",
+      #   "Spelunker",
+      #   "Tidepool Pupil",
+      #   "Mass Production",
+      #   "The Solarium",
+      #   "Clumsy Steward",
+      #   "Bloodpetal Biome",
+      #   "Sketch Artist",
+      #   "Tunnel Terror"
+      # ]) ->
+      #   :"Quest Warlock"
 
       any?(card_info, [
         "Raptor Herald",
@@ -52,45 +52,45 @@ defmodule Backend.PlayedCardsArchetyper.WarlockArchetyper do
       ]) ->
         :"Mill Warlock"
 
-      any?(card_info, [
-        "Corpsicle",
-        "Horizon's Edge",
-        "Sizzling Cinder",
-        "Dreamplanner Zephyrs",
-        "Wisp",
-        "Healthstone",
-        "Elven Archer",
-        "Platysaur",
-        "Helfire",
-        "Glacial Shard",
-        "Cursed Catacombs",
-        "The Solarium",
-        "Conflagrate",
-        "Hellfire"
-      ]) ->
-        :"Quest Warlock"
+      # any?(card_info, [
+      #   "Corpsicle",
+      #   "Horizon's Edge",
+      #   "Sizzling Cinder",
+      #   "Dreamplanner Zephyrs",
+      #   "Wisp",
+      #   "Healthstone",
+      #   "Elven Archer",
+      #   "Platysaur",
+      #   "Helfire",
+      #   "Glacial Shard",
+      #   "Cursed Catacombs",
+      #   "The Solarium",
+      #   "Conflagrate",
+      #   "Hellfire"
+      # ]) ->
+      #   :"Quest Warlock"
 
       any?(card_info, ["Ysera, Emerald Aspect", "Plated Beetle", "Drain Soul", "Sleepy Resident"]) ->
         :"Mill Warlock"
 
-      any?(card_info, [
-        "Ultralisk Cavern",
-        "Consume",
-        "Table Flip",
-        "Ancient of Yore",
-        "Eternal Layover",
-        "Cursed Campaign",
-        "Elise the Navigator",
-        "Bob the Bartender",
-        "Slithery Slope",
-        "Kil'jaeden",
-        "The Ceaseless Expanse",
-        "Wheel of DEATH!!!"
-      ]) ->
-        :"Starship Warlock"
+      # any?(card_info, [
+      #   "Ultralisk Cavern",
+      #   "Consume",
+      #   "Table Flip",
+      #   "Ancient of Yore",
+      #   "Eternal Layover",
+      #   "Cursed Campaign",
+      #   "Elise the Navigator",
+      #   "Bob the Bartender",
+      #   "Slithery Slope",
+      #   "Kil'jaeden",
+      #   "The Ceaseless Expanse",
+      #   "Wheel of DEATH!!!"
+      # ]) ->
+      #   :"Starship Warlock"
 
-      any?(card_info, ["Kerrigan, Queen of Blades"]) ->
-        :"Quest Warlock"
+      # any?(card_info, ["Kerrigan, Queen of Blades"]) ->
+      #   :"Quest Warlock"
 
       true ->
         :"Other Warlock"
