01fa7351a879523c3485cbad542225d2131650e2
Boris <boris.ivan.babic@gmail.com>
2025-09-01 19:22:32 +0200

    Move matchup popularity to top

--- lib/backend_web/components/matchups_table.ex
+++ lib/backend_web/components/matchups_table.ex
@@ -15,17 +15,20 @@ defmodule Components.MatchupsTable do
         <div class="notification is-warning">This UI is WIP (work in progress).</div>
         <table class="tw-text-black tw-border-collapse tw-table-fixed tw-text-center" :if={sorted_matchups = sort_matchups(@matchups)}>
           <thead class="tw-text-black">
-            <th class="tw-w-[10px]">Total Winrate</th>
-            <th class="tw-w-[10px]">Popularity</th>
-            <th class="tw-w-[50px]">Archetype</th>
+            <tr>
+            <th rowspan="2" class="tw-w-[10px]">Total Winrate</th>
+            <th rowspan="2" class="tw-w-[50px]">Archetype</th>
             <th :for={matchup <- sorted_matchups} class={"tw-w-[300px]", "tw-text-black", "class-background", Deck.extract_class(Matchups.archetype(matchup)) |> String.downcase()}>
               {Matchups.archetype(matchup)}
             </th>
+            </tr>
+            <tr :if={total_games = total_games(sorted_matchups)}>
+              <th :for={matchup <- sorted_matchups} class="tw-text-center tw-border tw-border-gray-600 tw-w-[10px] tw-bg-gray-500"> {Util.percent(Matchups.total_stats(matchup).games, total_games) |> Float.round(1)}%</th>
+            </tr>
           </thead>
           <tbody>
-            <tr class="tw-text-center tw-h-[25px] tw-truncate tw-text-clip" :for={matchup <- sorted_matchups} :if={total_games = total_games(sorted_matchups)}>
+            <tr class="tw-text-center tw-h-[25px] tw-truncate tw-text-clip" :for={matchup <- sorted_matchups} >
               <WinrateTag tag_name="td" class={"tw-text-center tw-border tw-border-gray-600 tw-w-[10px]"} :if={%{winrate: winrate, games: games} = Matchups.total_stats(matchup)} winrate={winrate} sample={games} />
-              <td class="tw-text-center tw-border tw-border-gray-600 tw-w-[10px] tw-bg-gray-500"> {Util.percent(Matchups.total_stats(matchup).games, total_games) |> Float.round(1)}</td>
               <td class={"tw-border", "tw-border-gray-600", "sticky-column", "class-background", Deck.extract_class(Matchups.archetype(matchup)) |> String.downcase()}>
                 {Matchups.archetype(matchup)}
               </td>
