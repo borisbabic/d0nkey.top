0b974add5b436a9edcdb4484b9e223c8a26fe4c5
Boris <boris.ivan.babic@gmail.com>
2025-11-11 08:48:43 +0100

    Add includes latest set filter

--- lib/backend/hearthstone.ex
+++ lib/backend/hearthstone.ex
@@ -783,9 +783,31 @@ defmodule Backend.Hearthstone do
 
   defp compose_decks_query({"exclude_cards", []}, query), do: query
 
+  defp compose_decks_query({"timeways", "yes"}, query) do
+    Hearthstone.timeways(query)
+  end
+
   defp compose_decks_query(unrecognized, query) do
     Logger.warning("Couldn't compose #{__MODULE__} query: #{inspect(unrecognized)}")
     query
+  end
+
+  def includes_latest_set(query, binding \\ :deck) do
+    timeways(query, binding)
+  end
+
+  def timeways(query, binding \\ :deck) do
+    card_ids = timeways_ids()
+    query |> where([{^binding, d}], fragment("? && ?", d.cards, ^card_ids))
+  end
+
+  def timeways_ids(filter_out \\ [120_646, 118_636, 119_314]),
+    do: filter_card_set(1957, filter_out)
+
+  def filter_card_set(card_set_id, filter_out_ids) do
+    for c <- CardBag.all_cards(),
+        c.card_set_id == card_set_id and c.id not in filter_out_ids,
+        do: c.id
   end
 
   def include_cards(query, cards, binding, field \\ :cards) do
