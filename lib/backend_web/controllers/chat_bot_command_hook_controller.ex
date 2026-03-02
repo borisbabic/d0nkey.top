649ce950817e925abf7f2a2a40cc982f21907c25
Boris Babic <boris.ivan.babic@gmail.com>
2026-03-03 00:41:19 +0100

    Add ldb top chat hook

--- lib/backend_web/controllers/chat_bot_command_hook_controller.ex
+++ lib/backend_web/controllers/chat_bot_command_hook_controller.ex
@@ -3,6 +3,8 @@ defmodule BackendWeb.ChatBotCommandHookController do
   use BackendWeb, :html_controller
   plug(:put_layout, false)
   alias Hearthstone.DeckcodeExtractor
+  alias Bot.LdbMessageHandler
+  alias Backend.Blizzard
   alias Backend.Infrastructure.BlizzardCommunicator, as: BlizzApi
 
   def deck_url(conn, %{"channel" => _, "deckcode" => deckcode_raw}) do
@@ -24,6 +26,59 @@ defmodule BackendWeb.ChatBotCommandHookController do
   def deck_url(conn, _params) do
     conn
     |> put_status(400)
+  end
+
+  def top_25(conn, %{
+        "channel" => _,
+        "default_region" => default_region,
+        "default_leaderboard_id" => default_leaderboard_id,
+        "options" => options
+      }) do
+    message = top_25_options_to_message(options, default_region, default_leaderboard_id)
+
+    conn
+    |> put_status(200)
+    |> text(message)
+  end
+
+  def top_25_options_to_message(options, default_region \\ "EU", default_leaderboard_id \\ "STD") do
+    options_fallbacks =
+      %{}
+      |> add_default_region(default_region)
+      |> add_default_leaderboard_id(default_leaderboard_id)
+
+    LdbMessageHandler.get_top_leaderboard_entries_season_info(
+      "!ldb-top " <> options,
+      options_fallbacks
+    )
+    |> create_top_25_message()
+  end
+
+  def create_top_25_message({entries, season_info}) do
+    count = Enum.count(entries)
+    leaderboard = season_info.leaderboard_id |> Blizzard.get_leaderboard_name(:long)
+    region = season_info.region |> Blizzard.get_region_name()
+
+    entries_part =
+      Enum.map_join(entries, " ", fn %{rank: rank, account_id: account_id} ->
+        "#{rank}. #{account_id}"
+      end)
+
+    "Top #{count} players #{region} - #{leaderboard}: #{entries_part}"
+  end
+
+  defp add_default_region(options, default) do
+    case Blizzard.to_region(default) do
+      {:ok, r} -> Map.put(options, :region, r)
+      _ -> options
+    end
+  end
+
+  defp add_default_leaderboard_id(options, default) do
+    case Blizzard.to_leaderboard_id(default) do
+      {:ok, id} -> Map.put(options, :leaderboard_id, id)
+      _ -> options
+    end
   end
 
   def next_reveal(conn, %{"channel" => _} = params) do
