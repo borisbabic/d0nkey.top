5ed48e8c63908affd69e6c661cb9e0ddd0779751
Boris Babic <boris.ivan.babic@gmail.com>
2026-03-17 07:20:36 +0100

    Update Druid CATA archetyping

--- lib/backend/deck_archetyper/druid_archetyper.ex
+++ lib/backend/deck_archetyper/druid_archetyper.ex
@@ -11,24 +11,6 @@ defmodule Backend.DeckArchetyper.DruidArchetyper do
       imbue_druid?(card_info) ->
         :"Imbue Druid"
 
-      menagerie?(card_info) ->
-        :"Menagerie Druid"
-
-      murloc?(card_info) ->
-        :"Murloc Druid"
-
-      starship?(card_info) and spell_damage_druid?(card_info) ->
-        :"SD Starship Druid"
-
-      copy_druid?(card_info) ->
-        :"Copy Druid"
-
-      owlonius_druid?(card_info) ->
-        :"Owlonius Druid"
-
-      spell_damage_druid?(card_info) ->
-        :"Spell Damage Druid"
-
       "Krona, Keeper of Eons" in card_info.card_names ->
         :"Krona Druid"
 
@@ -38,32 +20,14 @@ defmodule Backend.DeckArchetyper.DruidArchetyper do
       treant_druid?(card_info) ->
         :"Treant Druid"
 
-      protoss?(card_info, 4) ->
-        :"Protoss Druid"
-
-      "Huddle Up" in card_info.card_names ->
-        :"Huddle Up Druid"
-
-      "Travelmaster Dungar" in card_info.card_names ->
-        :"Dungar Druid"
-
       token?(card_info) ->
         :"Token Druid"
-
-      starship?(card_info) ->
-        :"Starship Druid"
-
-      "Hydration Station" in card_info.card_names ->
-        :"Hydration Druid"
-
-      greybough?(card_info) ->
-        :"Greybough Druid"
 
       bad?(card_info) ->
         :"Bad Druid"
 
-      "Sky Mother Aviana" in card_info.card_names ->
-        :"Aviana Druid"
+      "Merithra" in card_info.card_names ->
+        :"Merithra Druid"
 
       "Lady Azshara" in card_info.card_names ->
         :"Azshara Druid"
@@ -79,6 +43,8 @@ defmodule Backend.DeckArchetyper.DruidArchetyper do
   @wide_buff [
     # pseudo wide buff
     "Forest's Gift",
+    # kinda buff
+    "Ulfar",
     "Power of the Wild",
     "Overheat",
     "Cosmic Phenomenon",
@@ -99,38 +65,6 @@ defmodule Backend.DeckArchetyper.DruidArchetyper do
       "Thickhide Kodo",
       "Feral Rage"
     ])
-  end
-
-  defp copy_druid?(card_info) do
-    min_count?(card_info, 2, [
-      "Elise the Navigator",
-      "Zin-Azshari"
-    ]) and
-      min_count?(card_info, 1, [
-        "Welcome Home!",
-        "Scrapbooking Student"
-      ])
-  end
-
-  defp greybough?(card_info) do
-    "Greybough" in card_info.card_names and
-      match?({_, ["Hydration Station"]}, lowest_highest_cost_cards(card_info))
-  end
-
-  @non_owlonius_druid_sd_cards [
-    "Magical Dollhouse",
-    "Bottomless Toy Chest",
-    "Woodland Wonders",
-    "Go with the Flow",
-    "Chia Drake",
-    "Sparkling Phial" | neutral_spell_damage()
-  ]
-  defp owlonius_druid?(ci) do
-    "Owlonius" in ci.card_names and min_count?(ci, 2, @non_owlonius_druid_sd_cards)
-  end
-
-  defp spell_damage_druid?(ci) do
-    min_count?(ci, 4, @non_owlonius_druid_sd_cards)
   end
 
   defp treant_druid?(ci),
