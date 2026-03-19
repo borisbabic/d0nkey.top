474c28b033f07879f6551e5a1e8ee1b63631bd40
Boris Babic <boris.ivan.babic@gmail.com>
2026-03-19 09:50:04 +0100

    Add Shaman PC archetyping

--- lib/backend/played_cards_archetyper/shaman_archetyper.ex
+++ lib/backend/played_cards_archetyper/shaman_archetyper.ex
@@ -6,17 +6,51 @@ defmodule Backend.PlayedCardsArchetyper.ShamanArchetyper do
 
   @standard_config [
     {:"Quest Shaman", ["Spirit of the Mountain"]},
-    {:"Imbue Shaman",
+    {:"Harold Shaman",
      [
-       "Living Garden",
-       "Flutterwing Guardian",
-       "Bitterbloom Knight",
-       "Petal Picker",
-       "Aspect's Embrace",
-       "Resplendent Dreamweaver",
-       "Glowroot Lure",
-       "Plucky Podling"
+       "Shadowed Informant",
+       "For All Time",
+       "Twilight Egg",
+       "Elise the Navigator",
+       "Nightmare Lord Xavius",
+       "Whelp of the Infinite",
+       "Healing Rain",
+       "Carrier Whelp",
+       "Ceremonial Clash",
+       "Crackling Clopudstrider",
+       "Ultraxion",
+       "Ancient Raptor",
+       "Muradin, High King",
+       "Deathwing, Worldbreaker",
+       "Al'Akir, Lord of Storms",
+       "High King's Hammer",
+       "Envoy of the End",
+       "Avatar Form",
+       "Flight of the Firehawk",
+       "Skywall Sentinel",
+       "Cloud Serpent",
+       "Muradin's Last Stand",
+       "Primordial Overseer",
+       "Ritual of Power",
+       "Witch's Apprentice",
+       "Hex",
+       "Static Shock",
+       "Lightning Storm",
+       "Mother Duck",
+       "Wailing Vapor",
+       "Far Sight",
+       "Voltaic Burst",
+       "Glacial Shard"
+       # x (4) Ascendance
      ]}
+    # {:"Imbue Shaman",
+    #  [
+    #    "Flutterwing Guardian",
+    #    "Bitterbloom Knight",
+    #    "Petal Picker",
+    #    "Resplendent Dreamweaver",
+    #    "Glowroot Lure"
+    #  ]}
   ]
   @wild_config []
 
