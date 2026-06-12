21b7b2e8f2201658e2b2eac6d7d09cf56a6cfb15
Boris Babic <boris.ivan.babic@gmail.com>
2026-06-12 11:43:23 +0200

    Add Iyingdi support to OmniBar

---
+++ lib/backend_web/controllers/iyingdi_controller.ex
@@ -0,0 +1,12 @@
+defmodule BackendWeb.IyingdiController do
+  use BackendWeb, :controller
+  alias Backend.Hearthstone
+  alias Backend.Iyingdi
+
+  def lineups(conn, %{"set_id" => set_id}) do
+    Iyingdi.ensure_lineups(set_id)
+
+    conn
+    |> redirect(to: Iyingdi.lineup_url(set_id))
+  end
+end
